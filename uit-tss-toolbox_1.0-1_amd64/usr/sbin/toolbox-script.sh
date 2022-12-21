1b63356feeebcb07c1b2a5f6eeb4549c16937b81
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2022-12-21 08:50:30 -0600

    Update toolbox-script.sh

--- uit-tss-toolbox_1.0-1_amd64/usr/sbin/toolbox-script.sh
+++ uit-tss-toolbox_1.0-1_amd64/usr/sbin/toolbox-script.sh
@@ -33,9 +33,11 @@ function info {
 function exitMessage {
 	lines=$(tput lines)
 	tput reset
-	tput cup $(( lines - 5 ))
+	tput cup $(( lines - 7 ))
 	echo "${DIM}Press ${BOLD}${BLUE}CTRL + C${RESET}${DIM} at any time to exit UIT-TSS-TOOLBOX${RESET}"
 	echo "${DIM}If you have exited UIT-TSS-TOOLBOX and you want to restart it, press ${BOLD}${BLUE}CTRL + D${RESET}"
+	echo ""
+	echo echo "${DIM}UIT-TSS-TOOLBOX by Cameron Raschke (caraschke@uh.edu). ${RED}Go coogs!!${RESET}"
 	tput cup 0 0
 	echo ""
 	info
