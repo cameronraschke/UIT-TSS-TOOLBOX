2f48157181d9b59c59d32a3b064afdfb6e11bd21
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2023-01-29 15:58:41 -0600

    2023-01-29

--- uit-tss-toolbox_1.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/uit-tss-sql-configure.sh
+++ uit-tss-toolbox_1.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/uit-tss-sql-configure.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/php
+<?php
 
 #apt install mysql-server -y
 #mysqld --initialize-insecure
@@ -10,3 +11,7 @@
 #mysql --user="root" --password="UHouston!" --execute="CREATE USER 'laptops'@'%' IDENTIFIED BY 'UHouston\!';"
 #mysql --user="root" --password="UHouston!" --execute="ALTER USER 'laptops'@'%' IDENTIFIED BY 'UHouston\!';"
 #mysql --user="root" --password="UHouston!" --execute="GRANT ALL ON *.* TO 'laptops'@'%' WITH GRANT OPTION;"
+
+$sql = "CREATE TABLE clientstats (tagnumber VARCHAR(150) NOT NULL DEFAULT '000000',device_type VARCHAR(150) NOT NULL DEFAULT 'N/A',last_job_date VARCHAR(150) NOT NULL DEFAULT 'N/A',all_lastuuid VARCHAR(150) NOT NULL DEFAULT 'N/A',all_time VARCHAR(150) NOT NULL DEFAULT 'N/A',all_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',erase_time VARCHAR(150) NOT NULL DEFAULT 'N/A',erase_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_time VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',all_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',erase_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',disk VARCHAR(150) NOT NULL DEFAULT 'N/A',PRIMARY KEY(tagnumber) )";
+
+?>
