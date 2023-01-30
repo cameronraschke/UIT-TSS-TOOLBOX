5606474e37fcf01a8b28280ebc4af6048ca2c66d
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2023-01-30 00:04:35 -0600

    Update uit-tss-sql-configure.php

--- uit-tss-toolbox_1.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/uit-tss-sql-configure.php
+++ uit-tss-toolbox_1.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/uit-tss-sql-configure.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php
 <?php
+include('/var/lib/UIT-TSS-TOOLBOX/DB-connect-local.php');
 
 #apt install mysql-server -y
 #mysqld --initialize-insecure
@@ -13,7 +14,10 @@
 #mysql --user="root" --password="UHouston!" --execute="GRANT ALL ON *.* TO 'laptops'@'%' WITH GRANT OPTION;"
 
 $sql = "CREATE TABLE clientstats (tagnumber VARCHAR(150) NOT NULL DEFAULT '000000',device_type VARCHAR(150) NOT NULL DEFAULT 'N/A',last_job_date VARCHAR(150) NOT NULL DEFAULT 'N/A',all_lastuuid VARCHAR(150) NOT NULL DEFAULT 'N/A',all_time VARCHAR(150) NOT NULL DEFAULT 'N/A',all_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',erase_time VARCHAR(150) NOT NULL DEFAULT 'N/A',erase_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_time VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',all_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',erase_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',disk VARCHAR(150) NOT NULL DEFAULT 'N/A',PRIMARY KEY(tagnumber) )";
+$conn->query($sql);
 
 $sql = "CREATE TABLE serverstats (date VARCHAR(150) NOT NULL DEFAULT 'N/A',laptop_count VARCHAR(150) NOT NULL DEFAULT 'N/A',last_image_update VARCHAR(150) NOT NULL DEFAULT 'N/A',all_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',erase_jobs VARCHAR(150) NOT NULL DEFAULT 'N/A',all_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',clone_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',nvme_erase_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A',ssd_erase_avgtime VARCHAR(150) NOT NULL DEFAULT 'N/A', PRIMARY KEY(date) )";
+$conn->query($sql);
 
+$conn->close();
 ?>
