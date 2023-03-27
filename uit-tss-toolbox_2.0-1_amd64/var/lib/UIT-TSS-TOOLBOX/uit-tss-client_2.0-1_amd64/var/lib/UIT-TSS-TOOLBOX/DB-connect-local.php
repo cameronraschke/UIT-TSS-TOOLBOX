9da73a02472156f16089366b395271b046882de6
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2023-03-27 10:07:26 -0500

    2023-03-27

---
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/uit-tss-client_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/DB-connect-local.php
@@ -0,0 +1,16 @@
+<?php
+$servername = '10.0.0.1';
+$username = 'laptops';
+$password = 'UHouston!';
+$dbname = "laptopDB";
+
+global $conn;
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $conn -> connect_error;
+  exit();
+}
+
+?>
