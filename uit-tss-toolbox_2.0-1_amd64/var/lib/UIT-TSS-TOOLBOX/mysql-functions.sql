f980849845d24cb79b6ff0932989b0331b27f2eb
Cameron Raschke <cameron@plutomail.io>
2024-04-16 10:36:24 -0500

    Update mysql-functions.sql

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql-functions.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql-functions.sql
@@ -3,17 +3,19 @@ DELIMITER //
 CREATE PROCEDURE iterateDate(date1 date, date2 date)
 DETERMINISTIC
 BEGIN
-DROP TEMPORARY TABLE IF EXISTS tblResults;
-CREATE TEMPORARY TABLE IF NOT EXISTS tblResults (date DATE);
+DROP TEMPORARY TABLE IF EXISTS tbl_results;
+CREATE TEMPORARY TABLE IF NOT EXISTS tbl_results (date DATE);
 
 label1: LOOP
     IF date1 < date2 THEN
-        INSERT INTO tblResults (date) VALUES(date1);
+        INSERT INTO tbl_results (date) VALUES(date1);
         SET date1 = ADDDATE(date1, INTERVAL 1 DAY);
         ITERATE label1;
     END IF;
     LEAVE label1;
 END LOOP label1;
+
+SELECT date FROM tbl_results;
 
 END; //
 
