8808f3217ddd4028bb637398c446862b2e04eda4
Cameron Raschke <cameron@plutomail.io>
2024-04-16 13:04:36 -0500

    Update mysql-functions.php

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-functions.php
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-functions.php
@@ -1,31 +1,12 @@
 #!/usr/bin/php
 <?php
 
-#require('/var/lib/UIT-TSS-TOOLBOX/PDO-connect');
+require('/var/lib/UIT-TSS-TOOLBOX/PDO-connect');
 
-function select ($sql) {
-    $attempts = 0;
-
-do {
-    try {
-        $user = "laptops";
-        $pass = "UHouston!";
-        $pdo = new PDO('mysql:host=10.0.0.1;dbname=laptopDB', $user, $pass, array(
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => true,
-            PDO::ERRMODE_EXCEPTION => true
-        ));
-    } catch (PDOException $e) {
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
-        $attempts++;
-        sleep(1);
-        continue;
-    }
-    break;
-} while ($attempts < 10);
+function DBSelect ($sql) {
+    $sql = $pdo->prepare($sql);
+    $sql->execute();
     
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
     $row = $sql->fetchAll(PDO::FETCH_COLUMN);
     foreach ($row as $key => $value) {
         return $value;
