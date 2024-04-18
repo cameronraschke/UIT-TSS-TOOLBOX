ed14090a5e33b719412be02d585e141f12f2d263
Cameron Raschke <cameron@plutomail.io>
2024-04-18 09:10:26 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
@@ -22,3 +22,20 @@ SELECT date FROM tbl_results;
 END; //
 
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS iterateJobLabels;
+DELIMITER //
+CREATE PROCEDURE iterateJobLabels()
+DETERMINISTIC
+SELECT 'Job UUID','Tag Number','Ethernet Address','WiFi Address','Date','Datetime','Department','BIOS Vendor','BIOS Version','BIOS Last Update',
+    'BIOS Revision','BIOS Firmware','System Manufacturer','System Model','System Serial','System UUID',
+    'System SKU','Motherboard Manufacturer','Motherboard Serial','Chassis Manufcaturer',
+    'Chassis Type','Chassis Serial','Chassis Tag','CPU Manufacturer',
+    'CPU Model','CPU Max Speed','CPU Cores','CPU Threads','CPU Temp','RAM Serial','Battery Manufacturer',
+    'Battery Name','Battery Capacity','Battery Serial Number','Battery Manufacture Date','Battery Max Charge %','Battery Charge Cycles',
+    'Boot Time','Job Type','Did Sleep (Boolean)','Disk','Disk Type','Disk Size (GB)','Disk Model','Disk Serial','Disk Firmware',
+    'Disk Power on Hours','Disk Temperature','Disk Reads (TB)','Disk Writes (TB)',
+    'Total Time for Jobs','Erase Successful','Erase Mode','Total Erase Time','Disk Erased %',
+    'Clone Successful','Clone Mode','Total Clone Time','Master Image','Clone Server','Clone Image','Last Image Update'//
+
+DELIMITER ;
