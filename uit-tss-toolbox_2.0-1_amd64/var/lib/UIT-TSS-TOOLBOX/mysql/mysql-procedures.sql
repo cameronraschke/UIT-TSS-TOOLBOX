9176ce4f40ec7e4e09eb00eaafe7ef09db85a9ba
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2024-05-24 22:11:42 -0500

    Update mysql-procedures.sql

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
@@ -43,7 +43,7 @@ UNION
 system_sku, chassis_type, disk, disk_model, disk_type, CONCAT(disk_size, ' GB'), disk_serial, CONCAT(disk_writes, ' TB'), CONCAT(disk_reads, ' TB'), 
 CONCAT(disk_power_on_hours, 'hrs'), CONCAT(disk_temp, ' C'), disk_firmware, battery_model, battery_serial, CONCAT(battery_health, '%'), battery_charge_cycles, 
 CONCAT(battery_capacity, ' Wh'), battery_manufacturedate, cpu_manufacturer, cpu_model, CONCAT(cpu_maxspeed, ' Ghz'), cpu_cores, cpu_threads, CONCAT(cpu_temp, ' C'), 
-motherboard_manufacturer, motherboard_serial, bios_version, bios_date, bios_firmware, ram_serial, ram_capacity ,CONCAT(ram_speed, 'Mhz'), CONCAT(cpu_usage, '%'), 
+motherboard_manufacturer, motherboard_serial, bios_version, bios_date, bios_firmware, ram_serial, CONCAT(ram_capacity, ' GB') ,CONCAT(ram_speed, 'Mhz'), CONCAT(cpu_usage, '%'), 
 CONCAT(network_usage, 'mbps'), CONCAT(boot_time, 's'), REPLACE(erase_completed, '1', 'Yes'), erase_mode, CONCAT(erase_diskpercent, '%'), CONCAT(erase_time, 's'), 
 REPLACE(clone_completed, '1', 'Yes'), REPLACE(clone_master, '1', 'Yes'), CONCAT(erase_time, 's')
 FROM jobstats WHERE department = 'techComm' ORDER BY time DESC);
