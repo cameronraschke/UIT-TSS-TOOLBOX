f791ae23b257b68418e6d1c4a6aced04e914471b
Cameron Raschke <cameron@plutomail.io>
2024-06-26 17:19:08 -0500

    Update mysql-procedures.sql

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
@@ -353,7 +353,8 @@ SELECT tagnumber AS 'Tag',
     CONCAT(battery_charge, '%') AS 'Battery Charge',
     battery_status AS 'Battery Status',
     CONCAT(cpu_temp, '°C') AS 'CPU Temp',
-    CONCAT(disk_temp, '°C') AS 'Disk Temp'
+    CONCAT(disk_temp, '°C') AS 'Disk Temp',
+    CONCAT(watts_now, 'Watts') AS 'Power Draw'
     FROM remote WHERE present_bool = '1' OR status LIKE 'fail%' ORDER BY present DESC LIMIT 40;
     END; //
 
@@ -372,8 +373,7 @@ SELECT tagnumber AS 'Tag',
     CONCAT(battery_charge, '%') AS 'Battery Charge',
     battery_status AS 'Battery Status',
     CONCAT(cpu_temp, '°C') AS 'CPU Temp',
-    CONCAT(disk_temp, '°C') AS 'Disk Temp',
-    CONCAT(watts_now, 'Watts') AS 'Power Draw'
+    CONCAT(disk_temp, '°C') AS 'Disk Temp'
     FROM remote WHERE present_bool IS NULL ORDER BY present DESC LIMIT 10;
     END; //
 
