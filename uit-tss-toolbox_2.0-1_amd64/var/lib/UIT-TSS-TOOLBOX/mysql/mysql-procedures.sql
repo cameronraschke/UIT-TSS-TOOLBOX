85526b4d9c8573819bb9eb1c1ee312296f96bf0a
Cameron Raschke <cameron@plutomail.io>
2024-06-26 17:50:44 -0500

    Update mysql-procedures.sql

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
@@ -388,7 +388,7 @@ BEGIN
 SELECT CONCAT(ROUND(AVG(battery_charge), 0), '%') AS 'Avg. Battery Charge',
     CONCAT(ROUND(AVG(cpu_temp), 0), '°C') AS 'Avg. CPU Temp',
     CONCAT(ROUND(AVG(disk_temp), 0), '°C') AS 'Avg. Disk Temp',
-    CONCAT(ROUND(AVG(watts_now), 0), ' Watts') AS 'Avg. Power Draw'
+    CONCAT(ROUND(AVG(watts_now), 0), ' Watts') AS 'Avg. Power Draw',
     CONCAT(ROUND(SUM(watts_now), 0), ' Watts') AS 'Total Power Draw'
     FROM remote WHERE present_bool = '1';
     END; //
