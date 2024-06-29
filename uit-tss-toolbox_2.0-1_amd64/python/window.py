03861f210aa9a46dd9786c043b1ea6348da3de02
Cameron Raschke <cameron@plutomail.io>
2024-06-28 19:19:47 -0500

    Update window.py

--- uit-tss-toolbox_2.0-1_amd64/python/window.py
+++ uit-tss-toolbox_2.0-1_amd64/python/window.py
@@ -1,29 +1,30 @@
 import tkinter as tk
-# import mysql.connector
+import mysql.connector
 
-# def query_db():
-#     cnx = mysql.connector.connect(
-#     host="172.27.53.105",
-#     user="management",
-#     password="UHouston!",
-#     database="laptopDB"
-#     )
-#     cursor = cnx.cursor()
+result = "Nada"
+def query_db():
+    cnx = mysql.connector.connect(
+    host="172.27.53.105",
+    user="management",
+    password="UHouston!",
+    database="laptopDB"
+    )
+    cursor = cnx.cursor()
 
-#     query = ("SELECT tagnumber, system_serial, time FROM jobstats "
-#             "WHERE tagnumber = %s AND %s")
+    query = ("SELECT tagnumber, system_serial, time FROM jobstats "
+            "WHERE tagnumber = %s AND %s")
 
-#     tagnumber = "625958"
-#     system_serial = "5CD014DJ2D"
+    tagnumber = "625958"
+    system_serial = "5CD014DJ2D"
 
-#     cursor.execute(query, (tagnumber, system_serial))
+    cursor.execute(query, (tagnumber, system_serial))
 
-#     for (first_name, last_name, hire_date) in cursor:
-#         print("{}, {} is in the DB".format(
-#         tagnumber, system_serial))
+    for (first_name, last_name, hire_date) in cursor:
+        result = print("{}, {} is in the DB".format(
+        tagnumber, system_serial))
 
-#     cursor.close()
-#     cnx.close()
+    cursor.close()
+    cnx.close()
 
 window = tk.Tk()
 
@@ -32,21 +33,21 @@ window.title("UIT-TSS-TOOLBOX Management")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-txt_edit = tk.Text(window)
+results = tk.Label(window, text=result)
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(frm_buttons, text="Open")
+btn_query = tk.Button(frm_buttons, text="Query DB")
 btn_save = tk.Button(frm_buttons, text="Save As...")
 
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_query.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 
 frm_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
+results.grid(row=0, column=1, sticky="nsew")
 
-# btn_open = tk.Button(frm_buttons, text="Open", command=query_db)
+btn_query = tk.Button(frm_buttons, text="Query DB", command=query_db)
 btn_save = tk.Button(frm_buttons, text="Save As...")
 
 window.mainloop()
