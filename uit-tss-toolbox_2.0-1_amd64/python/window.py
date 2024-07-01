d4aa2191f91adfb70f9cd97c7ad9dc10c0d8532b
Cameron Raschke <cameron@plutomail.io>
2024-07-01 16:20:31 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/python/window.py
+++ uit-tss-toolbox_2.0-1_amd64/python/window.py
@@ -34,6 +34,30 @@ def cxn_query(stmt):
     cursor.close()
     cxn.close()
 
+def cxn_update(stmt):
+    cxn = mysql.connector.connect(
+    host="172.27.53.105",
+    user="management",
+    password="UHouston!",
+    database="laptopDB"
+    )
+    cursor = cxn.cursor()
+
+    cursor.execute(stmt)
+    results = cursor.fetchall()
+    reply.delete("1.0", tk.END)
+    nlines = 0
+    for (tagnumber) in results:
+        nlines = nlines + 1
+        print(tagnumber)
+        reply.insert("1.0", tagnumber)
+        reply.insert("1.0", "\n")
+    reply.insert("1.0", cursor.rowcount, "record(s) affected")
+    reply.insert("1.0", "\n")
+    cursor.close()
+    cxn.close()
+
+
 def cxn_connect():
     stmt="SELECT 'Connected to 172.27.53.105'"
     cxn_query(stmt)
@@ -42,13 +66,20 @@ def cxn_q_all():
     stmt="SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL GROUP BY tagnumber) AND tagnumber IS NOT NULL GROUP BY tagnumber"
     cxn_query(stmt)
 
+def cxn_update_b():
+    stmt=""
+    stmt="UPDATE remote SET task = 'update' WHERE tagnumber IN (SELECT location FROM locations WHERE time IN (SELECT MAX(time) FROM locations GROUP BY tagnumber) AND location = 'b'"
+    cxn_update(stmt)
+
 
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 btn_connect = tk.Button(frm_buttons, text="Connect...", command=cxn_connect)
 btn_query = tk.Button(frm_buttons, text="Query", command=cxn_q_all)
+btn_update_b = tk.Button(frm_buttons, text="Update Loc B", command=cxn_update_b)
 
 btn_connect.grid(row=0, column=0, sticky="ew", padx=5)
 btn_query.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+btn_update_b.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
 
 frm_buttons.grid(row=0, column=0, sticky="ns")
 reply.grid(row=0, column=1, sticky="nsew")
