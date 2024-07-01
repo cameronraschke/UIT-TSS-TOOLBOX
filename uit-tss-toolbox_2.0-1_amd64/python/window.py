0d3feeb1d4a83bb216e82cda8a3eb817a4daf567
Cameron Raschke <cameron@plutomail.io>
2024-07-01 16:55:28 -0500

    Update window.py

--- uit-tss-toolbox_2.0-1_amd64/python/window.py
+++ uit-tss-toolbox_2.0-1_amd64/python/window.py
@@ -8,12 +8,15 @@ window.title("UIT-TSS-TOOLBOX Management")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
+serverIP="172.27.53.105"
+
 
 reply = tk.Text(window)
 
 def cxn_query(stmt):
+    global serverIP
     cxn = mysql.connector.connect(
-    host="172.27.53.105",
+    host=serverIP,
     user="management",
     password="UHouston!",
     database="laptopDB"
@@ -35,8 +38,9 @@ def cxn_query(stmt):
     cxn.close()
 
 def cxn_update(stmt):
+    global serverIP
     cxn = mysql.connector.connect(
-    host="172.27.53.105",
+    host=serverIP,
     user="management",
     password="UHouston!",
     database="laptopDB"
@@ -45,6 +49,9 @@ def cxn_update(stmt):
 
     cursor.execute(stmt)
     cxn.commit()
+    nlines=f"({cursor.rowcount}, record(s) affected)"
+    reply.insert("1.0", f'Number of lines: {nlines}')
+    reply.insert("1.0", "\n")
     cursor.close()
     cxn.close()
 
@@ -58,18 +65,25 @@ def cxn_q_all():
     cxn_query(stmt)
 
 def cxn_update_b():
-    stmt="UPDATE remote SET task = 'update' WHERE tagnumber IN (SELECT location FROM locations WHERE time IN (SELECT MAX(time) FROM locations GROUP BY tagnumber) AND location = 'b')"
+    stmt="UPDATE remote SET task = 'update' WHERE tagnumber IN (SELECT tagnumber FROM locations WHERE time IN (SELECT MAX(time) FROM locations GROUP BY tagnumber) AND location = 'b')"
+    cxn_update(stmt)
+
+def cxn_update_b():
+    stmt="UPDATE remote SET task = 'update' WHERE tagnumber IN (SELECT tagnumber FROM locations WHERE time IN (SELECT MAX(time) FROM locations GROUP BY tagnumber) AND location = 'q')"
     cxn_update(stmt)
 
 
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_connect = tk.Button(frm_buttons, text="Connect...", command=cxn_connect)
-btn_query = tk.Button(frm_buttons, text="Query", command=cxn_q_all)
-btn_update_b = tk.Button(frm_buttons, text="Update Loc B", command=cxn_update_b)
 
-btn_connect.grid(row=0, column=0, sticky="ew", padx=5)
-btn_query.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-btn_update_b.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+btn_connect = tk.Button(frm_buttons, text="Test Conn", command=cxn_connect)
+btn_query = tk.Button(frm_buttons, text="Query All", command=cxn_q_all)
+btn_update_b = tk.Button(frm_buttons, text="Update Loc B", command=cxn_update_b)
+btn_update_q = tk.Button(frm_buttons, text="Update Loc Q", command=cxn_update_q)
+
+btn_connect.grid(row=1, column=0, sticky="ew", padx=5)
+btn_query.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+btn_update_b.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+btn_update_q.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
 
 frm_buttons.grid(row=0, column=0, sticky="ns")
 reply.grid(row=0, column=1, sticky="nsew")
