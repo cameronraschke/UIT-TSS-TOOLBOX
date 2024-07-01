f8b8e56cda9079452719931917786d2e35521f4c
Cameron Raschke <cameron@plutomail.io>
2024-07-01 15:20:17 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/python/window.py
+++ uit-tss-toolbox_2.0-1_amd64/python/window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import mysql.connector
+from mysql.connector import errorcode
 
 window = tk.Tk()
 
@@ -7,39 +8,44 @@ window.title("UIT-TSS-TOOLBOX Management")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-cnx = mysql.connector.connect(
-host="172.27.53.105",
-user="management",
-password="UHouston!",
-database="laptopDB"
-)
-
 
 reply = tk.Text(window)
-def cxn_query(sql):
-    cursor = cnx.cursor()
-    cursor.execute(sql)
+
+def cxn_query(stmt):
+    cxn = mysql.connector.connect(
+    host="172.27.53.105",
+    user="management",
+    password="UHouston!",
+    database="laptopDB"
+    )
+    cursor = cxn.cursor()
+
+    cursor.execute(stmt)
     results = cursor.fetchall()
     reply.delete("1.0", tk.END)
+    nlines = 0
     for (tagnumber) in results:
+        nlines = nlines + 1
         print(tagnumber)
         reply.insert("1.0", tagnumber)
         reply.insert("1.0", "\n")
+    reply.insert("1.0", f'Number of lines: {nlines}')
+    reply.insert("1.0", "\n")
     cursor.close()
-    cnx.close()
+    cxn.close()
 
 def cxn_connect():
-    sql="SELECT 'bruh'"
-    cxn_query(sql)
+    stmt="SELECT 'Connected to 172.27.53.105'"
+    cxn_query(stmt)
 
 def cxn_q_all():
-    sql="SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL GROUP BY tagnumber) AND tagnumber IS NOT NULL GROUP BY tagnumber"
-    cxn_query(sql)
+    stmt="SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL GROUP BY tagnumber) AND tagnumber IS NOT NULL GROUP BY tagnumber"
+    cxn_query(stmt)
 
 
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 btn_connect = tk.Button(frm_buttons, text="Connect...", command=cxn_connect)
-btn_query = tk.Button(frm_buttons, text="Query", command=cxn_q_all())
+btn_query = tk.Button(frm_buttons, text="Query", command=cxn_q_all)
 
 btn_connect.grid(row=0, column=0, sticky="ew", padx=5)
 btn_query.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
@@ -48,8 +54,3 @@ frm_buttons.grid(row=0, column=0, sticky="ns")
 reply.grid(row=0, column=1, sticky="nsew")
 
 window.mainloop()
-
-window.mainloop()
-
-
-
--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
@@ -389,12 +389,12 @@ CREATE PROCEDURE selectRemoteStats()
 DETERMINISTIC
 BEGIN
 SELECT 
-    (SELECT COUNT(tagnumber) FROM remote WHERE present_bool = '1') AS 'Total Present Laptops',
+    (SELECT COUNT(tagnumber) FROM remote WHERE present_bool = '1') AS 'Present Laptops',
     CONCAT(ROUND(AVG(battery_charge), 0), '%') AS 'Avg. Battery Charge',
     CONCAT(ROUND(AVG(cpu_temp), 1), '°C') AS 'Avg. CPU Temp',
     CONCAT(ROUND(AVG(disk_temp), 1), '°C') AS 'Avg. Disk Temp',
-    CONCAT(ROUND(AVG(watts_now), 0), ' Watts') AS 'Avg. Power Draw',
-    CONCAT(ROUND(SUM(watts_now), 0), ' Watts') AS 'Total Power Draw',
+    CONCAT(ROUND(AVG(watts_now), 0), ' Watts') AS 'Avg. Real Power Draw',
+    CONCAT(ROUND(SUM(watts_now), 0), ' Watts') AS 'Real Power Draw',
     CONCAT(ROUND(SUM(IF (battery_status NOT IN ('Disharging', ''), 55, NULL)), 0), ' Watts', ' (' , ROUND(SUM(IF (present_bool='1', 55, NULL)), 0), ' Watts Anticipated', ')') AS 'Power Draw from Wall'
     FROM remote WHERE present_bool = '1';
     END; //
