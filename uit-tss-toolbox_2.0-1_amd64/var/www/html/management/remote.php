cd22639e570cdc395b04bc6a3efcb82f7827b9a2
Cameron Raschke <cameron@plutomail.io>
2024-07-01 19:01:12 -0500

    Update

---
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
@@ -0,0 +1,10 @@
+<h1>Remote Table</h1>
+<h3>The remote table monitors laptops currently plugged into the UIT-TSS-TOOLBOX server.</h3>
+<?php
+require('./uit-sql-refresh-server');
+require('./mysql-functions');
+dbSelect("SELECT date FROM serverstats");
+foreach $arr as $key => $value {
+    echo "<p>$value</p>";
+}
+?>
