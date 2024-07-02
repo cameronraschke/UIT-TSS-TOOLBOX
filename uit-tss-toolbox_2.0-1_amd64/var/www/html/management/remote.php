5214f6287ac8e0e3796575bc49de80c7b90b6944
Cameron Raschke <cameron@plutomail.io>
2024-07-01 19:13:14 -0500

    Update remote.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
@@ -2,7 +2,7 @@
 <h3>The remote table monitors laptops currently plugged into the UIT-TSS-TOOLBOX server.</h3>
 <?php
 require('./uit-sql-refresh-remote');
-require('./mysql-functions');
+include('./mysql-functions');
 dbSelect("SELECT tagnumber FROM remote");
 foreach ($arr as $key => $value) {
     echo "<p>$value</p>";
