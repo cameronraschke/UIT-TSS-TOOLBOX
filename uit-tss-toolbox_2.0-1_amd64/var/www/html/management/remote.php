9619fe178fe6daf71868664dcafb7e70745f1334
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2024-07-02 20:26:10 -0500

    Update remote.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
@@ -2,42 +2,27 @@
 include('header.php');
 include('mysql/mysql-functions');
 ?>
+
 <html>
 <body>
 <h1>Remote Table</h1>
 <h3>The remote table monitors laptops currently plugged into the UIT-TSS-TOOLBOX server.</h3>
-<?php
 
-dbSelect("SELECT tagnumber FROM remote");
-foreach ($arr as $key => $value) {
-    echo "<p>$value[tagnumber]</p>" . PHP_EOL;
-}
-?>
 <?php
-
 echo "<table style='border-collapse: collapse' border='1'>";
 echo "<tr>";
-echo "<th>Name</th>";
-echo "<th>Date</th>";
-echo "<th>Price</th>";
-echo "<th>Payed</th>";
-echo "<th>Category</th>";
+echo "<th>Tagnumber</th>";
+echo "<th>Last Job Data</th>";
 echo "</tr>";
 
 dbSelect("SELECT * FROM remote WHERE present_bool = '1' ORDER BY date DESC");
 foreach ($arr as $key => $value) {
     $tagnumber = $row["tagnumber"];
-    #$price = $row["price"];
-    #$date = $row["date"];
-    #$payed = $row["payed"];
-    #$category = $row["category"];
+    $lastJob = $row["date"];
 
 	echo "<tr>";
 	echo "<td>$tagnumber</td>";
-	echo "<td>$date</td>";
-	echo "<td>\$$price</td>";
-	echo "<td>$payed</td>";
-	echo "<td>$category</td>";
+	echo "<td>$lastJob</td>";
 	echo "</tr>";
 
 }
