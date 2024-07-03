d44b7325ec243a0f447de15535e297476b579649
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2024-07-02 20:03:49 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/index.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/index.php
@@ -8,7 +8,7 @@
     <body>
         <div class='menubar'>
         <p><span style='float: left;'><a href='index.php'>Return Home</a></span></p>
-        <p><span style='float: right;'>Logged in as <b>$login_user</b>.</span></p>
+        <p><span style='float: right;'>Logged in as <b><?php echo "$login_user"; ?></b>.</span></p>
         <br>
         <p><span style='float: right;'>Not <b><?php echo "$login_user"; ?></b>? <a href='logout.php'>Click Here to Logout</a></p>
         </p>
--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
@@ -1,8 +1,7 @@
 <h1>Remote Table</h1>
 <h3>The remote table monitors laptops currently plugged into the UIT-TSS-TOOLBOX server.</h3>
 <?php
-require('./uit-sql-refresh-remote');
-include('./mysql-functions');
+include('mysql-functions');
 dbSelect("SELECT tagnumber FROM remote");
 foreach ($arr as $key => $value) {
     echo "<p>$value</p>";
@@ -11,14 +10,9 @@ foreach ($arr as $key => $value) {
 <?php
 include('mysql/mysql-functions');
 
-ob_start();
+header("refresh: 10");
 
-$due_sum = array();
-$sql = "select SUM(price) as due_sum from billing WHERE requester = 'cameron@plutomail.io' AND payed = 'No'";
-$results = $conn->query($sql);
-while ($row = $results->fetch_array(MYSQLI_ASSOC)) {
-        $due_sum = $row['due_sum'];
-}
+ob_start();
 
 echo "<table style='border-collapse: collapse' border='1'>";
 echo "<tr>";
@@ -32,11 +26,11 @@ echo "</tr>";
 $sql = "SELECT * FROM remote WHERE present_bool = '1' ORDER BY date DESC";
 $results = $pdo->query($sql);
 while ($row = $results->fetch_array(MYSQLI_ASSOC)) {
-        $name = $row["name"];
-        $price = $row["price"];
-        $date = $row["date"];
-        $payed = $row["payed"];
-        $category = $row["category"];
+        $tagnumber = $row["tagnumber"];
+        #$price = $row["price"];
+        #$date = $row["date"];
+        #$payed = $row["payed"];
+        #$category = $row["category"];
 
 if ($payed == "No") {
 	echo "<tr>";
@@ -59,18 +53,9 @@ echo "<tr>";
 echo "<td><b style='color: red;'>\$$due_sum<b></td>";
 echo "</tr>";
 echo "</table>";
-echo "<p><a href='https://web.plutomail.io/billing.php'>View Billing Console</a></p>";
 
 echo "</body></html>";
 $stdout = ob_get_contents();
 
-$conn->close();
-
-        $to_2fa = "billing@plutomail.io";
-        $subject_2fa = "Daily Billing Report";
-        $body_2fa = "<p>Here are the unpaid charges on the billing console:</p><br>$stdout<br><p>Want to dispute a charge? <a href='mailto:cameron@plutomail.io?Subject=Billing Charge Dispute'>Click Here</a>";
-        $headers_2fa = "From: \"Plutomail Billing\" <webmaster@plutomail.io> \r\nReply-To: cameron@plutomail.io \r\nMIME-Version: 1.0 \r\nContent-Type: text/html; charset=UTF-8 \r\n";
-
-	mail($to_2fa, $subject_2fa, $body_2fa, $headers_2fa);
 ob_end_clean();
 ?>
