242553dc00baf540d05cbc52ccbe9552d778a190
Cameron Raschke <cameron@plutomail.io>
2024-07-04 15:10:28 -0500

    Update locations.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
@@ -26,7 +26,7 @@ $time = $dt->format('Y-m-d H:i:s.v');
 
         <div class='styled-form2'>
             <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search tagnumber..." autofocus>
-            <input type="text" id="myInputLocations" onkeyup="myFunctionLocations()" placeholder="Search locations..." autofocus>
+            <input type="text" id="myInputLocations" onkeyup="myFunctionLocations()" placeholder="Search locations...">
         </div>
 
         <div class='styled-table'>
@@ -44,7 +44,7 @@ $time = $dt->format('Y-m-d H:i:s.v');
                 </thead>
                 <tbody>
 <?php
-dbSelect("SELECT tagnumber, system_serial, location, IF (status='0', 'Working', 'Broken') AS 'status', IF (os_installed='1', 'Yes', 'No') AS 'os_installed', note, CONVERT(time, DATETIME) AS 'time' FROM locations WHERE tagnumber IN (SELECT tagnumber FROM locations WHERE tagnumber IN (SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL AND department IS NOT NULL GROUP BY tagnumber) AND department = 'techComm')) AND time IN (SELECT MAX(time) FROM locations WHERE tagnumber IS NOT NULL GROUP BY tagnumber)");
+dbSelect("SELECT tagnumber, system_serial, location, IF (status='0', 'Working', 'Broken') AS 'status', IF (os_installed='1', 'Yes', 'No') AS 'os_installed', note, CONVERT(time, DATETIME) AS 'time' FROM locations WHERE tagnumber IN (SELECT tagnumber FROM locations WHERE tagnumber IN (SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL AND department IS NOT NULL GROUP BY tagnumber) AND department = 'techComm')) AND time IN (SELECT MAX(time) FROM locations WHERE tagnumber IS NOT NULL GROUP BY tagnumber) ORDER BY time DESC");
 foreach ($arr as $key => $value) {
     echo "<tr>" . PHP_EOL;
     echo "<td>" . $value['tagnumber'] . "</td>" . PHP_EOL;
