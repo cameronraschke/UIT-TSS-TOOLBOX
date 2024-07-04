bb9c6e567c557ff7ffc09856ca065daa7156b3dd
Cameron Raschke <cameron@plutomail.io>
2024-07-04 16:52:40 -0500

    Update update-locations.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/update-locations.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/update-locations.php
@@ -23,11 +23,28 @@ $time = $dt->format('Y-m-d H:i:s.v');
         <div class='pagetitle'><h1>Update Client Locations</h1></div>
         <div class='pagetitle'><h2>Here you can update all the information about a client's location and general status.</h2></div>
 
+
         <div class='location-form'>
-            <form>
-                <label for="tagnumber">Tag Number</label>
-                <input type="text" id="tagnumber" name="tagnumber">
-            </form>
+        <form method="post">
+        <?php
+        if ($_POST['tagnumber']) {
+            echo "<label for='tagnumber'>Tag Number</label>";
+            echo "<input type='text' id='tagnumber' name='tagnumber'>";
+            echo "<input type='submit' value='Search'>";
+            echo "<label for='serial'>Serial Number</label>";
+            echo "<input type='text' id='serial' name='serial' value='$systemSerial' readonly>";
+            echo "<br>";
+            echo "<label for='location'>Location</label>";
+            echo "<input type='text' id='location' name='location'>";
+        } else {
+            echo "<label for='tagnumber'>Tag Number</label>";
+            echo "<input type='text' id='tagnumber' name='tagnumber'>";
+            echo "<input type='submit' value='Search'>";
+            echo "<br>";
+        }
+
+        ?>
+        </form>
         </div>
     <div class="uit-footer">
         <img src="https://uh.edu/infotech/_images/_reorg-images/uh-2ndry-uit-artboard_horiz-reverse_black.svg">
