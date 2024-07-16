5cacb2363148be923771ea8485f3603bea6db75d
Cameron Raschke <cameron@plutomail.io>
2024-07-16 17:02:14 -0500

    Update remote.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
@@ -131,15 +131,15 @@ if (isset($_POST['location']) && isset($_POST['location-action'])) {
             <table id="myTable" width="100%">
             <thead>
                 <tr>
-                <th>Tagnumber</th>
-                <th>Last Heard</th>
-                <th>Pending Job</th>
-                <th>Current Status</th>
-                <th>Battery Charge</th>
-                <th>Battery Status</th>
-                <th>CPU Temp</th>
-                <th>Disk Temp</th>
-                <th>Actual Power Draw</th>
+                <th onclick="sortTable(0)">Tagnumber</th>
+                <th onclick="sortTable(1)">Last Heard</th>
+                <th onclick="sortTable(2)">Pending Job</th>
+                <th onclick="sortTable(3)">Current Status</th>
+                <th onclick="sortTable(4)">Battery Charge</th>
+                <th onclick="sortTable(5)">Battery Status</th>
+                <th onclick="sortTable(6)">CPU Temp</th>
+                <th onclick="sortTable(7)">Disk Temp</th>
+                <th onclick="sortTable(8)">Actual Power Draw</th>
                 </tr>
             </thead>
             <tbody>
@@ -272,6 +272,61 @@ echo "</div>";
             }
             }
         }
+        }
+
+        function sortTable(n) {
+            var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+            table = document.getElementById("myTable");
+            switching = true;
+            // Set the sorting direction to ascending:
+            dir = "asc";
+            /* Make a loop that will continue until
+            no switching has been done: */
+            while (switching) {
+                // Start by saying: no switching is done:
+                switching = false;
+                rows = table.rows;
+                /* Loop through all table rows (except the
+                first, which contains table headers): */
+                for (i = 1; i < (rows.length - 1); i++) {
+                // Start by saying there should be no switching:
+                shouldSwitch = false;
+                /* Get the two elements you want to compare,
+                one from current row and one from the next: */
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                /* Check if the two rows should switch place,
+                based on the direction, asc or desc: */
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                    }
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                    }
+                }
+                }
+                if (shouldSwitch) {
+                /* If a switch has been marked, make the switch
+                and mark that a switch has been done: */
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                // Each time a switch is done, increase this count by 1:
+                switchcount ++;
+                } else {
+                /* If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again. */
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+                }
+            }
         }
     </script>
     <div class="uit-footer">
