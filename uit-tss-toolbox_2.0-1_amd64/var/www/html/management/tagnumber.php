9930a7be8e9c677ce6d576fed2601df74e1d8d87
Cameron Raschke <cameron@plutomail.io>
2024-07-26 18:01:39 -0500

    Update tagnumber.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
@@ -34,9 +34,7 @@ $time = $dt->format('Y-m-d H:i:s.v');
         ?>
         </div>
         
-        <div class='pagetitle'>
-            <h3>General Client Info</h3>
-        </div>
+        <div class='pagetitle'><h3>General Client Info</h3></div>
 
         <div class='styled-table' style="width: auto; height:10%; overflow:auto; margin: 1% 1% 0% 1%;">
         <table width="100%">
@@ -63,10 +61,49 @@ $time = $dt->format('Y-m-d H:i:s.v');
                 }
                 ?>
             </tbody>
+        </table>
+        </div>
 
+        <div class='pagetitle'><h3>Job Info</h3></div>
+        <div class='styled-table' style="width: auto; height:20%; overflow:auto; margin: 1% 1% 0% 1%;">
+        <table width="100%">
+            <thead>
+                <tr>
+                <th>Time</th>
+                <th>UUID</th>
+                <th>CPU Usage</th>
+                <th>Network Usage</th>
+                <th>Boot Time</th>
+                <th>Erase Completed</th>
+                <th>Erase Mode</th>
+                <th>Erase Time Elapsed</th>
+                <th>Clone Completed</th>
+                <th>Clone Master</th>
+                <th>Clone Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                dbSelect("SELECT DATE_FORMAT(time, '%b %D %Y, %r') AS 'time_formatted', uuid, CONCAT(cpu_usage, '%') AS 'cpu_usage', CONCAT(network_usage, ' mbps') AS 'network_usage', CONCAT(boot_time, 's'), IF (erase_completed = 1, 'Yes', 'No') AS 'erase_completed', erase_mode, SEC_TO_TIME(erase_time) AS 'erase_time', IF (clone_completed = 1, 'Yes', 'No') AS clone_completed, IF (clone_master = 1, 'Yes', 'No') AS clone_master, SEC_TO_TIME(clone_time) AS 'clone_time' WHERE tagnumber = '" . $_GET['tagnumber'] . "' AND host_connected = '1' AND (erase_completed = '1' OR clone_completed = '1') ORDER BY time DESC LIMIT 10");
+                foreach ($arr as $key=>$value) {
+                    echo "<tr>" . PHP_EOL;
+                    echo "<td>" . $value['time_formatted'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['uuid'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['cpu_usage'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['network_usage'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['boot_time'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['erase_completed'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['erase_mode'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['erase_time'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['clone_completed'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['clone_master'] . "</td>" . PHP_EOL;
+                    echo "<td>" . $value['clone_time'] . "</td>" . PHP_EOL;
+                    echo "</tr>" . PHP_EOL;
+                }
+                ?>
+            </tbody>
         </table>
         </div>
 
     </body>
 </html>
-?>
