c34cca10e32786d33448ebc498c8163aa65ea3aa
cameronraschke <cameron@plutomail.io>
2024-07-28 00:53:14 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/july2024-migrate.php
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/july2024-migrate.php
@@ -19,13 +19,66 @@ include('/var/lib/UIT-TSS-TOOLBOX/mysql-functions');
 
 dbSelect("SELECT tagnumber FROM jobstats WHERE tagnumber IS NOT NULL GROUP BY tagnumber");
 foreach ($arr as $key=>$value) {
+    echo "Working on" . $tagNum . PHP_EOL;
     $tagNum = $value['tagnumber'];
-    dbSelectVal("SELECT tagnumber FROM system_data WHERE tagnumber = '" . $tagNum . "'");
-    if ($result == "NULL") {
+
+    // Check if tag already exists, if not then insert it
+    dbSelectVal("SELECT tagnumber AS 'result' FROM system_data WHERE tagnumber = '" . $tagNum . "'");
+    if (filter($result) == 1) {
         dbInsertSystemData("$tagNum");
     }
-    dbSelectVal("SELECT wifi_mac FROM jobstats WHERE wifi_mac IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+
+    // wifi_mac
+    dbSelectVal("SELECT wifi_mac AS 'result' FROM jobstats WHERE wifi_mac IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
     dbUpdateSystemData("$tagNum", "wifi_mac", $result);
+
+    // system_manufacturer
+    dbSelectVal("SELECT system_manufacturer AS 'result' FROM jobstats WHERE system_manufacturer IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "system_manufacturer", $result);
+
+    // system_model
+    dbSelectVal("SELECT system_model AS 'result' FROM jobstats WHERE system_model IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "system_model", $result);
+
+    // system_uuid
+    dbSelectVal("SELECT system_uuid AS 'result' FROM jobstats WHERE system_uuid IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "system_uuid", $result);
+
+    // system_sku
+    dbSelectVal("SELECT system_sku AS 'result' FROM jobstats WHERE system_sku IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "system_sku", $result);
+
+    // chassis_type
+    dbSelectVal("SELECT chassis_type AS 'result' FROM jobstats WHERE chassis_type IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "chassis_type", $result);
+
+    // cpu_manufacturer
+    dbSelectVal("SELECT cpu_manufacturer AS 'result' FROM jobstats WHERE cpu_manufacturer IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "cpu_manufacturer", $result);
+
+    // cpu_model
+    dbSelectVal("SELECT cpu_model AS 'result' FROM jobstats WHERE cpu_model IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "cpu_model", $result);
+    
+    // cpu_maxspeed
+    dbSelectVal("SELECT cpu_maxspeed AS 'result' FROM jobstats WHERE cpu_maxspeed IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "cpu_maxspeed", $result);
+
+    // cpu_cores
+    dbSelectVal("SELECT cpu_cores AS 'result' FROM jobstats WHERE cpu_cores IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "cpu_cores", $result);
+
+    // cpu_threads
+    dbSelectVal("SELECT cpu_threads AS 'result' FROM jobstats WHERE cpu_threads IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "cpu_threads", $result);
+
+    // motherboard_manufacturer
+    dbSelectVal("SELECT motherboard_manufacturer AS 'result' FROM jobstats WHERE motherboard_manufacturer IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "motherboard_manufacturer", $result);
+
+    // motherboard_serial
+    dbSelectVal("SELECT motherboard_serial AS 'result' FROM jobstats WHERE motherboard_serial IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "motherboard_serial", $result);
 }
 
 
