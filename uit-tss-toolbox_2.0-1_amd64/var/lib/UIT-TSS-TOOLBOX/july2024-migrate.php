c13e8b497d27b342c020e5390566e159331c8334
cameronraschke <cameron@plutomail.io>
2024-07-28 00:36:02 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/july2024-migrate.php
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/july2024-migrate.php
@@ -20,7 +20,10 @@ include('/var/lib/UIT-TSS-TOOLBOX/mysql-functions');
 dbSelect("SELECT tagnumber FROM jobstats WHERE tagnumber IS NOT NULL GROUP BY tagnumber");
 foreach ($arr as $key=>$value) {
     $tagNum = $value['tagnumber'];
-    dbInsertSystemData("$tagNum");
+    dbSelectVal("SELECT tagnumber FROM system_data WHERE tagnumber = '" . $tagNum . "'");
+    if ($result == "NULL") {
+        dbInsertSystemData("$tagNum");
+    }
     dbSelectVal("SELECT wifi_mac FROM jobstats WHERE wifi_mac IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
     dbUpdateSystemData("$tagNum", "wifi_mac", $result);
 }
