0801603c823f79d3cf396c2c39922978dea1afce
cameronraschke <cameron@plutomail.io>
2024-07-28 00:31:23 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/july2024-migrate.php
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/july2024-migrate.php
@@ -17,11 +17,12 @@ motherboard_serial
 */
 include('/var/lib/UIT-TSS-TOOLBOX/mysql-functions');
 
-dbSelect("SELECT tagnumber, wifi_mac, system_manufacturer, system_model, system_uuid, system_sku, chassis_type, cpu_manufacturer, cpu_model, cpu_maxspeed, cpu_cores, cpu_threads, motherboard_manufacturer, motherboard_serial FROM jobstats WHERE tagnumber IS NOT NULL GROUP BY tagnumber");
+dbSelect("SELECT tagnumber FROM jobstats WHERE tagnumber IS NOT NULL GROUP BY tagnumber");
 foreach ($arr as $key=>$value) {
     $tagNum = $value['tagnumber'];
     dbInsertSystemData("$tagNum");
-    dbUpdateSystemData("$tagNum", "wifi_mac", $value["wifi_mac"]);
+    dbSelectVal("SELECT wifi_mac FROM jobstats WHERE wifi_mac IS NOT NULL AND tagnumber = '" . $tagNum . "' ORDER BY time DESC LIMIT 1");
+    dbUpdateSystemData("$tagNum", "wifi_mac", $result);
 }
 
 
