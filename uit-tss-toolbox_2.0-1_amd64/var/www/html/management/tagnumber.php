10a212d3072dec635705a19ef91a43ae09cb758d
Cameron Raschke <cameron@plutomail.io>
2024-07-30 13:11:51 -0500

    Update tagnumber.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
@@ -49,7 +49,7 @@ if (isset($_POST['task'])) {
                     <?php
                     if ($_GET['tagnumber']) {
                         dbSelect("SELECT (CASE WHEN task = 'update' THEN 'Update' WHEN task = 'nvmeErase' THEN 'Erase Only' WHEN task = 'hpEraseAndClone' THEN 'Erase + Clone' WHEN task = 'findmy' THEN 'Play Sound' WHEN task = 'hpCloneOnly' THEN 'Clone Only' WHEN task IS NULL THEN 'No Job' END) AS 'formatted_task', task FROM remote");
-                        echo "<option value='" . $_GET["tagnumber"] . "|" . $value["task"] . "'>" . $value["formatted_task"] . "</option>";
+                        echo "<option value='" . $_GET["tagnumber"] . "|" . $value["task"] . "'>" . $value['formatted_task'] . "</option>";
                         echo "<option value='" . $_GET["tagnumber"] . "|update'>Update</option>";
                         echo "<option value='" . $_GET["tagnumber"] . "|nvmeErase'>Erase Only</option>";
                         echo "<option value='" . $_GET["tagnumber"] . "|hpCloneOnly'>Clone Only</option>";
