e1d8f78807fb16fe4c5e0838bfc18b4016c748d1
Cameron Raschke <cameron@plutomail.io>
2024-07-31 15:08:54 -0500

    Update tagnumber.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
@@ -149,13 +149,12 @@ if (isset($_POST['task'])) {
             </thead>
             <tbody>
                 <?php
-                dbSelect("SELECT DATE_FORMAT(time, '%b %D %Y, %r') AS 'time_formatted', CONCAT(cpu_usage, '%') AS 'cpu_usage', CONCAT(network_usage, ' mbps') AS 'network_usage', CONCAT(boot_time, 's'), IF (erase_completed = 1, 'Yes', 'No') AS 'erase_completed', erase_mode, SEC_TO_TIME(erase_time) AS 'erase_time', IF (clone_completed = 1, 'Yes', 'No') AS clone_completed, IF (clone_master = 1, 'Yes', 'No') AS clone_master, SEC_TO_TIME(clone_time) AS 'clone_time', bios_version FROM jobstats WHERE tagnumber = '" . $_GET['tagnumber'] . "' AND host_connected = '1' AND (erase_completed = '1' OR clone_completed = '1') ORDER BY time DESC LIMIT 10");
+                dbSelect("SELECT DATE_FORMAT(time, '%b %D %Y, %r') AS 'time_formatted', CONCAT(cpu_usage, '%') AS 'cpu_usage', CONCAT(network_usage, ' mbps') AS 'network_usage', IF (erase_completed = 1, 'Yes', 'No') AS 'erase_completed', erase_mode, SEC_TO_TIME(erase_time) AS 'erase_time', IF (clone_completed = 1, 'Yes', 'No') AS clone_completed, IF (clone_master = 1, 'Yes', 'No') AS clone_master, SEC_TO_TIME(clone_time) AS 'clone_time', bios_version FROM jobstats WHERE tagnumber = '" . $_GET['tagnumber'] . "' AND host_connected = '1' AND (erase_completed = '1' OR clone_completed = '1') ORDER BY time DESC LIMIT 10");
                 foreach ($arr as $key=>$value) {
                     echo "<tr>" . PHP_EOL;
                     echo "<td>" . $value['time_formatted'] . "</td>" . PHP_EOL;
                     echo "<td>" . $value['cpu_usage'] . "</td>" . PHP_EOL;
                     echo "<td>" . $value['network_usage'] . "</td>" . PHP_EOL;
-                    echo "<td>" . $value['boot_time'] . "</td>" . PHP_EOL;
                     echo "<td>" . $value['erase_completed'] . "</td>" . PHP_EOL;
                     echo "<td>" . $value['erase_mode'] . "</td>" . PHP_EOL;
                     echo "<td>" . $value['erase_time'] . "</td>" . PHP_EOL;
