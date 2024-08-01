90579bb75f2611eb9bb5bcf799bb451ea45f6b2e
Cameron Raschke <cameron@plutomail.io>
2024-08-01 14:41:01 -0500

    Update locations.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
@@ -254,6 +254,16 @@ if ($_GET["location"]) {
                 </thead>
                 <tbody>
 <?php
+if ($_GET["location"]) {
+    $sql = "SELECT tagnumber, system_serial, location, IF ((status='0' OR status IS NULL), 'Working', 'Broken') AS 'status', IF (os_installed='1', 'Yes', 'No') AS 'os_installed', note, DATE_FORMAT(time, '%b %D %Y, %r') AS 'time_formatted' FROM locations WHERE tagnumber IN (SELECT tagnumber FROM locations WHERE tagnumber IN (SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL AND department IS NOT NULL GROUP BY tagnumber) AND department IN ('techComm', 'property', 'shrl'))) AND time IN (SELECT MAX(time) FROM locations WHERE tagnumber IS NOT NULL GROUP BY tagnumber) AND location = :location ORDER BY time DESC";
+    $stmt = $pdo->prepare($sql);
+    $sqlLocation = htmlspecialchars_decode($_GET['location']);
+    $stmt->bindParam(':location', $sqlLocation, PDO::PARAM_STR);
+    $stmt->execute();
+    $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else {
+    dbSelect("SELECT tagnumber, system_serial, location, IF ((status='0' OR status IS NULL), 'Working', 'Broken') AS 'status', IF (os_installed='1', 'Yes', 'No') AS 'os_installed', note, DATE_FORMAT(time, '%b %D %Y, %r') AS 'time_formatted' FROM locations WHERE tagnumber IN (SELECT tagnumber FROM locations WHERE tagnumber IN (SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL AND department IS NOT NULL GROUP BY tagnumber) AND department IN ('techComm', 'property', 'shrl'))) AND time IN (SELECT MAX(time) FROM locations WHERE tagnumber IS NOT NULL GROUP BY tagnumber) ORDER BY time DESC");
+}
 foreach ($arr as $key => $value) {
     echo "<tr>" . PHP_EOL;
     dbSelectVal("SELECT present_bool AS 'result' FROM remote WHERE tagnumber = '" . $value["tagnumber"] . "'");
