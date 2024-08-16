8d5b0f38a65650562a62c7a02134e4d2cb3035f8
Raschke <caraschk@CougarNet.UH.EDU>
2024-08-16 12:34:32 -0500

    Update remote.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
@@ -262,7 +262,7 @@ echo "</div>";
             </thead>
 
 <?php
-$db->select("SELECT tagnumber, DATE_FORMAT(present, '%b %D %Y, %r') AS 'time_formatted', status, CONCAT(battery_charge, '%') AS 'battery_charge', battery_status, CONCAT(cpu_temp, '°C') AS 'cpu_temp',  CONCAT(disk_temp, '°C') AS 'disk_temp', CONCAT(watts_now, ' Watts') AS 'watts_now' FROM remote WHERE present_bool IS NULL ORDER BY present DESC, task DESC, status ASC, tagnumber DESC");
+$db->select("SELECT tagnumber, DATE_FORMAT(present, '%b %D %Y, %r') AS 'time_formatted', status, CONCAT(battery_charge, '%') AS 'battery_charge', battery_status, CONCAT(cpu_temp, '°C') AS 'cpu_temp',  CONCAT(disk_temp, '°C') AS 'disk_temp', CONCAT(watts_now, ' Watts') AS 'watts_now' FROM remote WHERE present_bool IS NULL ORDER BY bios_updated DESC, kernel_updated DESC, present DESC, task DESC, status ASC, tagnumber DESC");
 if (arrFilter($db->get()) === 0) {
     foreach ($db->get() as $key => $value) {
         echo "<tr>";
