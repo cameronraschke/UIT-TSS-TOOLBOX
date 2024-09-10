e522817e58903bdfb5dceeb6cde8a993d706ba71
Raschke <caraschk@CougarNet.UH.EDU>
2024-09-10 09:28:38 -0500

    Update locations.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
@@ -339,7 +339,7 @@ if (isset($_GET["location"])) {
         $db->select("SELECT tagnumber, system_serial, location, IF ((status='0' OR status IS NULL), 'Working', 'Broken') AS 'status', IF (os_installed='1', 'Yes', 'No') AS 'os_installed', IF (bios_updated = '1', 'Yes', 'No') AS 'bios_updated', note, DATE_FORMAT(time, '%b %D %Y, %r') AS 'time_formatted' FROM locations WHERE tagnumber IN (SELECT tagnumber FROM locations WHERE tagnumber IN (SELECT tagnumber FROM jobstats WHERE time IN (SELECT MAX(time) FROM jobstats WHERE tagnumber IS NOT NULL AND department IS NOT NULL GROUP BY tagnumber) AND department IN ('techComm', 'property', 'shrl'))) AND time IN (SELECT MAX(time) FROM locations WHERE tagnumber IS NOT NULL GROUP BY tagnumber) ORDER BY time DESC");
     }
 }
-if (arrFilter($db->get()) === 0 && isset($db->get()) === TRUE) {
+if (arrFilter($db->get()) === 0) {
     $arr = $db->get();
 }
 foreach ($arr as $key => $value) {
