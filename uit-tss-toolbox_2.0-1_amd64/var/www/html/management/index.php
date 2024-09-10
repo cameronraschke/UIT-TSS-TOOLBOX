4b57152e0e70666c0e7ca048401df685f613abb8
Raschke <caraschk@CougarNet.UH.EDU>
2024-09-10 11:15:35 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/index.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/index.php
@@ -16,11 +16,11 @@
 
         <div class='pagetitle' style="text-align:center;"><h1 style="margin:auto;">TechComm Laptop Management Site</h1></div>
         <div class='pagetitle'><h2>Welcome, <?php echo $login_user; ?>.</h2></div>
-        <div class='page-content'><h3><a href="remote.php">Remote Management and Live Overview</a></h3></div>
-        <div class='page-content'><h3><a href="locations.php">Update and View Client Locations</a></h3></div>
-        <div class='page-content'><h3><a href="serverstats.php">Daily Reports</a></h3></div>
-        <div class='page-content'><h3><a href="clientstats.php">Client Report</a></h3></div>
-        <div class='page-content'><h3><a href="reports.php">Generate and Download Reports (WIP)</a></h3></div>
+        <div class='page-content'><h3><a href="/remote">Remote Management and Live Overview</a></h3></div>
+        <div class='page-content'><h3><a href="/locations.php">Update and View Client Locations</a></h3></div>
+        <div class='page-content'><h3><a href="/serverstats.php">Daily Reports</a></h3></div>
+        <div class='page-content'><h3><a href="/clientstats.php">Client Report</a></h3></div>
+        <div class='page-content'><h3><a href="/reports.php">Generate and Download Reports (WIP)</a></h3></div>
 
         <div class="uit-footer">
             <img src="/images/uh-footer.svg">
