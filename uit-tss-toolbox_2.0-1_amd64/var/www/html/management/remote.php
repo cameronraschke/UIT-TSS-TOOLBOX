d6d13f66a34acfe916bb856614a4a582d9e6c7ac
Raschke <caraschk@CougarNet.UH.EDU>
2024-09-16 15:07:51 -0500

    Update remote.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/remote.php
@@ -30,11 +30,7 @@ $db = new db();
                         position: { my: "right+50%", at: "top+50%", of: window }
                     });
                 document.getElementById('popup-' + tag).style.display = {style: "block"};
-
-                
-                setTimeout(function() {
-                    fetchHTML(tag);
-                }, 3000)
+                fetchHTML(tag);
             }
         </script>
 
