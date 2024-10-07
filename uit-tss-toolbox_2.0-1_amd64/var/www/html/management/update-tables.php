0efb83e8b131427e0638b58a8c4a317b2e41e9ae
Raschke <caraschk@CougarNet.UH.EDU>
2024-10-07 14:41:55 -0500

    Update

---
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/update-tables.php
@@ -0,0 +1,7 @@
+<?php
+    include('/var/www/html/management/php/include.php');
+    include('/var/www/html/management/php/uit-sql-refresh-client');
+    include('/var/www/html/management/php/uit-sql-refresh-location');
+    include('/var/www/html/management/php/uit-sql-refresh-remote');
+    include('/var/www/html/management/php/uit-sql-refresh-server');
+?>
