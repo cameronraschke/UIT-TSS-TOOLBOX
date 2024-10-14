6c2a8caa03cfbc8391f1e82f531eb8388bd84be5
Raschke <caraschk@CougarNet.UH.EDU>
2024-10-14 14:16:14 -0500

    Update update-tables.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/update-tables.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/update-tables.php
@@ -1,10 +1,6 @@
 <?php
     require('/var/www/html/management/header.php');
-    include('/var/www/html/management/php/include.php');
-    include('/var/www/html/management/php/uit-sql-refresh-client');
-    include('/var/www/html/management/php/uit-sql-refresh-location');
-    include('/var/www/html/management/php/uit-sql-refresh-remote');
-    include('/var/www/html/management/php/uit-sql-refresh-server');
+    require('/var/www/html/management/php/include.php');
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +9,29 @@
         <link rel='stylesheet' type='text/css' href='/css/main.css' />
         <title>Update Table Data - UIT Client Mgmt</title>
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <style>
+            h3 {
+                margin: 2% 1% 1% 2%;
+            }
+            p {
+                margin: 1% 1% 1% 2%;
+            }
+        </style>
 </head>
 <body>
-<h3 style="margin: 2% 1% 1% 2%">Updating tables<h3>
+<h3>Updating tables<h3>
+    <p>Updating Client Statistics...</p>
+    <?php include('/var/www/html/management/php/uit-sql-refresh-client'); ?>
+    <p><i>Done!</i></p>
+    <p>Updating Location Data... </p>
+    <?php include('/var/www/html/management/php/uit-sql-refresh-location'); ?>
+    <p><i>Done!</i></p>
+    <p>Updating Remote Job Data... </p>
+    <?php include('/var/www/html/management/php/uit-sql-refresh-remote'); ?>
+    <p><i>Done!</i></p>
+    <p>Updating Daily Reports Data... </p>
+    <?php include('/var/www/html/management/php/uit-sql-refresh-server'); ?>
+    <p><i>Done!</i></p>
 </body>
 
 </html>
