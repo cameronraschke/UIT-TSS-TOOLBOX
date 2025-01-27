b281bb34a076be5b1592d41941196abf8558cba4
cameronraschke <110272354+cameronraschke@users.noreply.github.com>
2025-01-27 12:03:40 -0600

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/locations.php
@@ -65,6 +65,62 @@ $db = new db();
             });
         } );
     </script>
+    <script>
+        function sortTable(n) {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById("myTable2");
+        switching = true;
+        // Set the sorting direction to ascending:
+        dir = "asc";
+        /* Make a loop that will continue until
+        no switching has been done: */
+        while (switching) {
+            // Start by saying: no switching is done:
+            switching = false;
+            rows = table.rows;
+            /* Loop through all table rows (except the
+            first, which contains table headers): */
+            for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /* Check if the two rows should switch place,
+            based on the direction, asc or desc: */
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+                }
+            }
+            }
+            if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount ++;
+            } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+            }
+        }
+        }
+        </script>
         <div class='menubar'>
             <p><span style='float: left;'><a href='index.php'>Return Home</a></span></p>
             <p><span style='float: right;'>Logged in as <b><?php echo htmlspecialchars($login_user, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401, "UTF-8", FALSE); ?></b>.</span></p>
@@ -372,15 +428,15 @@ unset($value1);
             <table id="myTable">
                 <thead>
                 <tr>
-                    <th>Tag Number</th>
-                    <th>System Serial</th>
-                    <th>Location</th>
-                    <th>Department</th>
-                    <th>Status</th>
-                    <th>OS Installed</th>
-                    <th>BIOS Updated</th>
-                    <th>Note</th>
-                    <th>Time</th>
+                    <th onclick="sortTable(0)">Tag Number</th>
+                    <th onclick="sortTable(1)">System Serial</th>
+                    <th onclick="sortTable(2)">Location</th>
+                    <th onclick="sortTable(3)">Department</th>
+                    <th onclick="sortTable(4)">Status</th>
+                    <th onclick="sortTable(5)">OS Installed</th>
+                    <th onclick="sortTable(6)">BIOS Updated</th>
+                    <th onclick="sortTable(7)">Note</th>
+                    <th onclick="sortTable(8)">Time</th>
                 </tr>
                 </thead>
                 <tbody>
