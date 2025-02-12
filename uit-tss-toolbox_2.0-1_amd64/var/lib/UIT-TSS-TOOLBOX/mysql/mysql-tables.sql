0ae2fde1b1b2ab5f31230558d7391ca0dc2041bd
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-02-12 14:51:51 -0600

    Update mysql-tables.sql

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-tables.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-tables.sql
@@ -220,7 +220,7 @@ INSERT INTO static_bios_stats
     )
     VALUES
     ('HP ProBook 450 G6', 'R71 Ver. 01.29.00'),
-    ('Latitude 7400', '1.36.0'),
+    ('Latitude 7400', '1.37.0'),
     ('Latitude 3500', '1.35.1'),
     ('Latitude 3560', 'A19'),
     ('Latitude 3590', '1.26.0'),
