0c6a19e59f32cb6004925778e3623fedb7533019
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-02-26 15:09:47 -0600

    Update mysql-procedures.sql

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
@@ -440,13 +440,13 @@ CREATE PROCEDURE iterateSHRLCSV()
 DETERMINISTIC
 BEGIN
 
-(SELECT 'Last Entry', 'Tag Number', 'Serial Number', 'System Model', 'Department', 'Location', 'CPU Model', 'CPU Cores', 'RAM Capacity', 'Disk Type', 'Disk Health', 'Note')
+(SELECT 'Last Entry', 'Tag Number', 'Serial Number', 'System Model', 'Department', 'Location', 'CPU Model', 'CPU Cores', 'RAM Capacity', 'Disk Size', 'Disk Type', 'Disk Health', 'Note')
 UNION
 (SELECT 
   locations.time, jobstats.tagnumber, jobstats.system_serial, 
   system_data.system_model, static_departments.department_readable, 
   locations.location, system_data.cpu_model, system_data.cpu_cores,
-  CONCAT(t1.ram_capacity, 'GB'), t1.disk_type, t1.disk_size, 
+  CONCAT(t1.ram_capacity, 'GB'), CONCAT(t1.disk_size, 'GB'), t1.disk_type,
   CONCAT(clientstats.disk_health, '%'), locations.note
 FROM jobstats 
 LEFT JOIN locations ON jobstats.tagnumber = locations.tagnumber 
