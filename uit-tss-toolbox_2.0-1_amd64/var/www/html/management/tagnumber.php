02806d0203a1adff9702e4d59d33b154129ebbfb
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-03-12 10:30:21 -0500

    Update tagnumber.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/tagnumber.php
@@ -312,7 +312,7 @@ WHERE jobstats.tagnumber IS NOT NULL and jobstats.system_serial IS NOT NULL
         }
         unset($value1);
       }
-      $db->Pselect("SELECT job, job_readable FROM static_job_names WHERE job_html_bool = 1 AND NOT job IN (SELECT IF(job_queued IS NULL, "", job_queued) FROM remote WHERE tagnumber = :tagnumber) ORDER BY job_rank ASC", array(':tagnumber' => $_GET["tagnumber"]));
+      $db->Pselect("SELECT job, job_readable FROM static_job_names WHERE job_html_bool = 1 AND NOT job IN (SELECT IF(job_queued IS NULL, '', job_queued) FROM remote WHERE tagnumber = :tagnumber) ORDER BY job_rank ASC", array(':tagnumber' => $_GET["tagnumber"]));
       foreach ($db->get() as $key => $value2) {
         echo "<option value='" . htmlspecialchars($value2["job"]) . "'>" . htmlspecialchars($value2["job_readable"]) . "</option>";
       }
