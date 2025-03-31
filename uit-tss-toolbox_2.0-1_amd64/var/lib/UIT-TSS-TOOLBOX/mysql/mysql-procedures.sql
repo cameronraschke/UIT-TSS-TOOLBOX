6253d72f47d1e72425f3d103489aab0a5b708627
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-03-31 08:01:38 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/var/lib/UIT-TSS-TOOLBOX/mysql/mysql-procedures.sql
@@ -533,16 +533,16 @@ END; //
 DELIMITER ;
 
 
-DROP PROCEDURE IF EXISTS iterateTempProperty;
+DROP PROCEDURE IF EXISTS iterateCustomReport;
 DELIMITER //
-CREATE PROCEDURE iterateTempProperty()
+CREATE PROCEDURE iterateCustomReport()
 DETERMINISTIC
 BEGIN
 
-(SELECT 'Tag Number', 'Serial Number', 'System Model', 'Disk Removed', 'Location', 'Last Update')
+(SELECT 'Tag Number', 'Serial Number', 'System Model', 'Disk Removed', 'Location', 'Note', 'Last Update')
 UNION ALL
 (SELECT locations.tagnumber, locations.system_serial, system_data.system_model, 
-	IF (locations.disk_removed = 1, 'Yes', 'No') AS 'disk_removed', locationFormatting(locations.location) AS 'location', locations.time
+	IF (locations.disk_removed = 1, 'Yes', 'No') AS 'disk_removed', locationFormatting(locations.location) AS 'location', locations.note, locations.time
 FROM locations
 LEFT JOIN departments ON locations.tagnumber = departments.tagnumber
 INNER JOIN (SELECT MAX(time) AS 'time' FROM departments GROUP BY tagnumber) t2 ON departments.time = t2.time
