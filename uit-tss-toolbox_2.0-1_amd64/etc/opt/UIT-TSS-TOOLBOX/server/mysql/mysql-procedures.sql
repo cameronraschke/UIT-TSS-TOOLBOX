b875a941bc6b2ae6c8fbb2a56d78919a1795f7f4
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-05-27 14:01:59 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/etc/opt/UIT-TSS-TOOLBOX/server/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/etc/opt/UIT-TSS-TOOLBOX/server/mysql/mysql-procedures.sql
@@ -582,6 +582,24 @@ END; //
 DELIMITER ;
 
 
+DROP PROCEDURE IF EXISTS iterateCheckoutName;
+DELIMITER //
+CREATE PROCEDURE iterateCheckoutName()
+DETERMINISTIC
+BEGIN
+
+(SELECT 'Tag Number', 'Serial Number', 'System Model', 'Checkout Date', 'Return Date', 'Note', 'Last Entry')
+UNION ALL
+(SELECT * FROM (SELECT checkout.time, checkout.tagnumber, checkout.customer_name, checkout.customer_psid, checkout.checkout_date, checkout.return_date, checkout.checkout_bool, checkout.note,
+    ROW_NUMBER() OVER (PARTITION BY tagnumber ORDER BY time DESC) AS 'row_nums' 
+    FROM checkout) t1
+    WHERE (t1.checkout_date IS NOT NULL OR t1.return_date) IS NOT NULL 
+        AND t1.row_nums <= 1 AND NOT t1.row_nums IS NULL AND t1.checkout_bool = 1 AND t1.customer_name = 'Blake Mudd' ORDER BY t1.time DESC, t1.customer_name ASC, t1.tagnumber DESC);
+
+END; //
+DELIMITER ;
+
+
 
 DROP PROCEDURE IF EXISTS selectNullTagnumbers;
 DELIMITER //
