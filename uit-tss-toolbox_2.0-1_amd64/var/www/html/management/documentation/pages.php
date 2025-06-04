d8b340315b6a4422626d13bd5e64d5b1dd2083d0
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-06-04 15:23:08 -0500

    Update pages.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/management/documentation/pages.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/management/documentation/pages.php
@@ -26,5 +26,37 @@ $db = new db();
     </div>
 
     <div class='pagetitle'><h1>Documentation - <?php echo htmlspecialchars($_GET["page"]); ?></h1></div>
+    <div class='documentation'>
+      <?php
+        if ($_GET["page"] == "locations") {
+          echo "<h3>Update Client Information</h3>";
+          echo "<p>
+            The page <a href='/locations.php'>locations.php</a> allows you to update multiple aspects of a client. 
+            On the left side of the webpage, you will see a box to enter a tag number. 
+            Once you enter the tag number, you will be able to modify information about the client. 
+            For new clients that have never been entered into the database, there will be no information filled out. This means that you must at least fill out the tag number, serial number, and location. 
+            Additionally, for existing clients, the pre-filled information provided will be based off of the most recent information the database has on the client.
+            </p>
+            <p>
+            For checkouts, the form will pre-fill the last known customer name and checkout date if the client has not been returned. 
+            A returned client is one that has a <i>Return Date</i> that is earlier or equal to the current date.
+            </p>";
+          echo "</br>";
+          echo "<h3>Advanced Filtering</h3>";
+          echo "<p>
+            The filtering form on the right side of the page allows you to creat advanced searches that look through every client in the database. 
+            You can select multiple values to create advanced filters and the form will tell you how many clients your filter returned. 
+            For a given filter in a dropdown menu, then number in parentheses denotes how many clients have that aspect. For example, look at the following:
+          </p>";
+          echo "<div class='location-form'><select><option>Tech Commons (82)</option><option>SHRL (33)</option></select></div>";
+          echo "<p>
+            This dropdown menu would denote that 82 clients are in the Tech Commons department and 33 clients are in SHRL.
+          </p>";
+        }
+      ?>
+    </div>
+    <div class="uit-footer">
+      <img src="/images/uh-footer.svg">
+    </div>
   </body>
 </html>
