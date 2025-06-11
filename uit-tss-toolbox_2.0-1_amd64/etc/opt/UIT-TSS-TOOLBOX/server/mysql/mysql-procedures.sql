572f751c2c275f007e0587aebc173332a39fb7f8
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-06-11 10:57:36 -0500

    Update

--- uit-tss-toolbox_2.0-1_amd64/etc/opt/UIT-TSS-TOOLBOX/server/mysql/mysql-procedures.sql
+++ uit-tss-toolbox_2.0-1_amd64/etc/opt/UIT-TSS-TOOLBOX/server/mysql/mysql-procedures.sql
@@ -250,8 +250,8 @@ DROP USER IF EXISTS 'cameron'@'localhost';
 CREATE USER IF NOT EXISTS 'cameron'@'localhost' IDENTIFIED BY 'UHouston!';
 GRANT ALL ON *.* TO 'cameron'@'localhost' WITH GRANT OPTION;
 
-DROP USER IF EXISTS 'uitclients'@'10.0.0.0/255.255.0.0';
-CREATE USER IF NOT EXISTS 'uitclients'@'10.0.0.0/255.255.0.0' IDENTIFIED BY 'UHouston!';
+DROP USER IF EXISTS 'uitclient'@'10.0.0.0/255.255.0.0';
+CREATE USER IF NOT EXISTS 'uitclient'@'10.0.0.0/255.255.0.0' IDENTIFIED BY 'UHouston!';
 GRANT SELECT, INSERT, UPDATE, EXECUTE ON laptopDB.* TO 'uitclients'@'10.0.0.0/255.255.0.0';
 
 DROP USER IF EXISTS 'uitweb'@'localhost';
