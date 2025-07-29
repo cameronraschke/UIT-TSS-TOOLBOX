af375fd4ae2fcb5dbd3e7071c8cd3d6dcc20a6e2
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-07-29 11:38:34 -0500

    Update locations.php

--- uit-tss-toolbox_2.0-1_amd64/var/www/html/uit-web/locations.php
+++ uit-tss-toolbox_2.0-1_amd64/var/www/html/uit-web/locations.php
@@ -131,12 +131,12 @@ if (isset($_POST["tagnumber"]) && isset($_POST['serial']) && isset($_POST["locat
   unset($value1);
 
   //Printing
-  if ($_POST["print"] == "TRUE") {
-    $tagNum = escapeshellcmd($_POST["tagnumber"]);
-    $customerName = escapeshellcmd($_POST["customer_name"]);
-    $checkoutDate = escapeshellcmd($_POST["checkout_date"]);
+  if ($_POST["print"] == "1") {
+    $tagNum = ($_POST["tagnumber"]);
+    $customerName = ($_POST["customer_name"]);
+    $checkoutDate = ($_POST["checkout_date"]);
     //$customerPSID = escapeshellcmd($_POST["customer_psid"]);
-    $returnDate = escapeshellcmd($_POST["return_date"]);
+    $returnDate = ($_POST["return_date"]);
 
     $dbPSQL->Pselect("SELECT TO_CHAR(:checkoutDate, 'MM/DD/YY') AS checkout_date", array(':checkoutDate' => $checkoutDate));
     foreach ($dbPSQL->get() as $key => $value1) {
@@ -150,7 +150,7 @@ if (isset($_POST["tagnumber"]) && isset($_POST['serial']) && isset($_POST["locat
     }
     unset($value1);
 
-    System("bash /var/www/html/uit-web/bash/uit-print-pdf.sh" . " " . escapeshellarg("WEB_SVC_PASSWD") . " " . escapeshellarg($tagNum) . " " . escapeshellarg($customerName) . " " . escapeshellarg($checkoutDate) . " " . escapeshellarg($customerPSID) . " " . escapeshellarg($returnDate));
+    System("bash /var/www/html/uit-web/bash/uit-print-pdf" . " " . escapeshellarg("WEB_SVC_PASSWD") . " " . escapeshellarg($tagNum) . " " . escapeshellarg($customerName) . " " . escapeshellarg($checkoutDate) . " " . escapeshellarg($customerPSID) . " " . escapeshellarg($returnDate));
   }
 
   unset($_POST);
