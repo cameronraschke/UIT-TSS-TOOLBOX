066092f01f77ea10e8c5368eb52dedfce859dfc2
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-07-31 15:46:18 -0500

    Update main.go

--- uit-tss-toolbox_2.0-1_amd64/opt/uit-toolbox/api/main.go
+++ uit-tss-toolbox_2.0-1_amd64/opt/uit-toolbox/api/main.go
@@ -10,25 +10,69 @@ import (
   "log"
   "time"
   "encoding/json"
+  "net/url"
+  "errors"
 
   "github.com/jackc/pgx/v5"
 )
 
 
-var serverTime = time.Now()
-
 var conn *pgx.Conn
 
+func urlToSql(requestURL string) (string, string, error) {
+    var sql string
+    var path string
+    var tagnumber string
+    var queries url.Values
 
-func db_query() (string, error) {
-  dbConnString := "postgres://uitweb:WEB_SVC_PASSWD@127.0.0.1:5432/uitdb?sslmode=disable"
-  conn, err := pgx.Connect(context.Background(), dbConnString)
+    parsedURL, err := url.Parse(requestURL)
+    if err != nil {
+      log.Print("Cannot parse URL: " + requestURL)
+    }
+
+    path = parsedURL.Path
+
+    RawQuery := parsedURL.RawQuery
+    queries, _ = url.ParseQuery(RawQuery)
+
+    tagnumber = queries.Get("tagnumber")
+
+    if path == "/api/remote" && queries.Get("type") == "live_image"  {
+      sql = "SELECT TO_CHAR(time, 'MM/DD/YY HH12:MI:SS AM') AS time_formatted, screenshot FROM live_images WHERE tagnumber = $1"
+    } else {
+      return "", "", errors.New("Bad URL request")
+    }
+
+      return sql, tagnumber, nil
+}
+
+func apiFunction (w http.ResponseWriter, req *http.Request) {
+  // requestURL := string(req.URL.Scheme + req.URL.Host + req.URL.Path + req.URL.RawQuery)
+  requestURL := req.URL.RequestURI()
+  log.Print("[" + time.Now().Format("01-02-2006 15:04:05") + "] " + "Request to " + requestURL)
+  sqlCode, tagnumner, err := urlToSql(requestURL)
   if err != nil {
+    log.Print("Cannot parse URL: ")
+  }
+
+  log.Print(sqlCode)
+
+  // Connect to DB
+  dbConnString := "postgres://uitweb:66cd42b7988c1d55954b23eab1ba5d73@127.0.0.1:5432/uitdb?sslmode=disable"
+  conn, err := pgx.Connect(context.Background(), dbConnString)
+  if err != nil  {
     fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
     os.Exit(1)
   }
-
-  rows, err := conn.Query(context.Background(), "SELECT tagnumber, system_serial, time FROM (SELECT tagnumber, system_serial, time, ROW_NUMBER() OVER (PARTITION BY tagnumber ORDER BY time DESC) AS row_nums FROM locations) s1 WHERE s1.row_nums = 1")
+  //   if ($_GET["type"] == "job_queue" && isset($_GET["tagnumber"])) { 
+  //   $dbPSQL->Pselect("SELECT remote.present_bool, remote.kernel_updated, client_health.bios_updated, remote.status AS remote_status, TO_CHAR(remote.present, 'MM/DD/YY HH12:MI:SS AM') AS remote_time_formatted FROM remote LEFT JOIN client_health ON remote.tagnumber = client_health.tagnumber WHERE remote.tagnumber = :tagnumber", array(':tagnumber' => htmlspecialchars($_GET["tagnumber"])));
+  //   foreach ($dbPSQL->get() as $key => $value) {
+  //     $event = "server_time";
+  //     $data = json_encode($value);
+  //   }
+  //   unset($value);
+  // }
+  rows, err := conn.Query(context.Background(), sqlCode, tagnumner)
     if err != nil {
       log.Print("Query error: %v\n", err)
     }
@@ -58,25 +102,23 @@ func db_query() (string, error) {
         log.Print(err)
     }
 
-    return string(jsonData), err
+    w.Header().Set("Content-Type", "application/json")
+    io.WriteString(w, string(jsonData))
 }
 
+
 func main() {
+  // Check if connection is valid
 
-	apiHandler := func(w http.ResponseWriter, req *http.Request) {
-    output, err := db_query()
-    if err != nil {
-      log.Print("Function returned error")
-    }
-		io.WriteString(w, output)
-	}
 
-	http.HandleFunc("/api", apiHandler)
-	log.Print("Server time: " + serverTime.Format("01-02-2006 15:04:05"))
+  // Route to correct function
+  mux := http.NewServeMux()
+  mux.HandleFunc("/api/", apiFunction)
+
+	log.Print("Server time: " + time.Now().Format("01-02-2006 15:04:05"))
 	log.Print("Starting web server on https://localhost:8080")
-	httpServerErr := http.ListenAndServeTLS("127.0.0.1:8080", "/etc/ssl/certs/uit-web.crt", "/etc/ssl/private/uit-web.key", nil)
-	log.Print(httpServerErr)
-	if (httpServerErr != nil) {
-		log.Printf("Listening on https://localhost:8080")
-	}
+
+	log.Fatal(http.ListenAndServeTLS("127.0.0.1:8080", "/etc/ssl/certs/uit-web.crt", "/etc/ssl/private/uit-web.key", mux))
+
+	log.Printf("Listening on https://localhost:8080")
 }
