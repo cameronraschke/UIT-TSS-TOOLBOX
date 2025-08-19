b063c934c385a5934d401bb68a78487d974dc38d
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-08-18 19:54:28 -0500

    Update get-framebuffer.c

--- uit-tss-toolbox_2.0-1_amd64/srv/uit-toolbox/client/src/uit-toolbox-client_2.0-1_amd64/usr/sbin/get-framebuffer.c
+++ uit-tss-toolbox_2.0-1_amd64/srv/uit-toolbox/client/src/uit-toolbox-client_2.0-1_amd64/usr/sbin/get-framebuffer.c
@@ -10,28 +10,35 @@
 
 
 int main () {
-    long int screensize = 0;
-    char *fb_buff = 0;
+    uint8_t *fb_data = NULL;
+    uint8_t *rgb888_buffer = NULL;
     int x = 0, y = 0;
-    long int location = 0;
 
     struct fb_var_screeninfo vinfo;
     struct fb_fix_screeninfo finfo;
 
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    FILE *outfile;
+    JSAMPROW row_pointer[1];
+    int jpeg_quality = 100;
+
+
     int fbfd = open("/dev/fb0", O_RDONLY);
     if (fbfd < 0) {
-            perror("Can't open /dev/fb0");
-            return 1;
+        perror("Can't open /dev/fb0");
+        return 1;
     }
 
     if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo)) {
         printf("%s\n", "Cannot read fixed screen info");
+        close(fbfd);
         return 1;
     }
 
-
     if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo)) {
         printf("%s\n", "Cannot read variable screen info");
+        close(fbfd);
         return 1;
     }
 
@@ -39,44 +46,66 @@ int main () {
     int fb_height = vinfo.yres;
     int fb_bits_per_pixel = vinfo.bits_per_pixel;
     int fb_bytes_per_pixel = fb_bits_per_pixel / 8;
-    //long int fb_size = fixedFbInfo.smem_len;
-    long int fb_size = fb_width * fb_height * fb_bytes_per_pixel;
+    long int fb_size = finfo.smem_len;
+    // long int fb_size = fb_width * fb_height * fb_bytes_per_pixel;
 
-
-    // Get bytes of screen for mmap
-    //long int screenBytes = screenWidth * screenHeight * (bytesPerPixel / 8);
-    //off_t screenBytes = lseek(fbfd, 0, SEEK_END);
-    fb_buff = (char *)mmap(0, fb_size, PROT_READ, MAP_SHARED, fbfd, 0);
-    if ((long int)fb_buff == -1) {
-            perror("Error mapping framebuffer");
-            close(fbfd);
-            return 1;
-    }
-    printf("Screen resolution: %dx%d (%ld bytes)\n", fb_width, fb_height, fb_size);
-
-    // Image conversion
-    struct jpeg_compress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-    FILE *outfile = fopen("/tmp/test-jpeg.jpeg", "wb");
-    JSAMPROW row_pointer[1];
-    int row_stride;
-    int jpeg_quality = 100;
-
-    cinfo.err = jpeg_std_error(&jerr);
-    //jerr.error_exit = jpeg_error_exit;
-    jpeg_create_compress(&cinfo);
-
-    if (outfile == NULL) {
-        perror("Cannot open jpeg output");
-        munmap(fb_buff, fb_size);
+    if (vinfo.bits_per_pixel != 32) {
+        fprintf(stderr, "Error: Expected 32 bits per pixel, but found %d.\n", fb_bits_per_pixel);
         close(fbfd);
         return 1;
     }
 
+    fb_data = (uint8_t *)mmap(0, fb_size, PROT_READ, MAP_SHARED, fbfd, 0);
+    if (fb_data == MAP_FAILED) {
+        perror("Error mmapping framebuffer");
+        close(fbfd);
+        return 1;
+    }
+    printf("Screen resolution: %dx%d (%ld bytes)\n", fb_width, fb_height, fb_size);
+
+    // Image conversion
+    rgb888_buffer = (uint8_t *)malloc(fb_width * fb_height * 3);
+    if (!rgb888_buffer) {
+        perror("Failed to allocate RGB888 buffer");
+        free(rgb888_buffer);
+        munmap(fb_data, fb_size);
+        close(fbfd);
+        return 1;
+    }
+
+    uint32_t *src_pixel_ptr = (uint32_t *)fb_data;
+    uint8_t *dest_pixel_ptr = rgb888_buffer;
+
+    int jpeg_processing_range_y = fb_height / 2;
+    int jpeg_processing_range_x = fb_width / 2;
+    for (int y = 0; y < fb_height; y++) {
+        for (int x = 0; x < fb_width; x++) {
+            uint32_t pixel_value = src_pixel_ptr[x];
+            // Take last 2 bits off 8-bit pixels to convert to 24-bit color.
+            *dest_pixel_ptr++ = (uint8_t)((pixel_value >> vinfo.red.offset) & 0xFF);
+            *dest_pixel_ptr++ = (uint8_t)((pixel_value >> vinfo.green.offset) & 0xFF);
+            *dest_pixel_ptr++ = (uint8_t)((pixel_value >> vinfo.blue.offset) & 0xFF);
+        }
+        src_pixel_ptr = (uint32_t *)((uint8_t *)src_pixel_ptr + finfo.line_length);
+    }
+
+    outfile = fopen("/tmp/test-jpeg.jpeg", "wb");
+    if (!outfile) {
+        perror("Error opening output JPEG file");
+        free(rgb888_buffer);
+        munmap(fb_data, fb_size);
+        close(fbfd);
+        return 1;
+    }
+
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
     jpeg_stdio_dest(&cinfo, outfile);
+
     cinfo.image_width = fb_width;
     cinfo.image_height = fb_height;
-    cinfo.input_components = 3; //R, G, B
+    cinfo.input_components = 3; // R, G, B
     cinfo.in_color_space = JCS_RGB;
 
     jpeg_set_defaults(&cinfo);
@@ -84,41 +113,18 @@ int main () {
 
     jpeg_start_compress(&cinfo, TRUE);
 
-    row_stride = fb_width * 3; //R, G, B
-
-    unsigned char *rgb_row_buffer = (unsigned char *)malloc(row_stride);
-    if (!rgb_row_buffer) {
-        perror("Cannot allocate row buffer");
-        jpeg_destroy_compress(&cinfo);
-        fclose(outfile);
-        munmap(fb_buff, fb_size);
-        close(fbfd);
-        return 1;
-    }
-
+    dest_pixel_ptr = rgb888_buffer;
     while (cinfo.next_scanline < cinfo.image_height) {
-        for (x = 0; x < vinfo.xres; x++) {
-            location = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) +
-                    (cinfo.next_scanline + vinfo.yoffset) * finfo.line_length;
-            if (vinfo.bits_per_pixel == 32 || vinfo.bits_per_pixel == 24) {
-                rgb_row_buffer[x * 3 + 0] = fb_buff[location + 2]; // Red
-                rgb_row_buffer[x * 3 + 1] = fb_buff[location + 1]; // Green
-                rgb_row_buffer[x * 3 + 2] = fb_buff[location + 0]; //Red
-            } else {
-                perror("Cannot read non-32 bit colors");
-                break;
-            }
-        }
-        row_pointer[0] = rgb_row_buffer;
-        (void)jpeg_write_scanlines(&cinfo, row_pointer, 1);
+        row_pointer[0] = dest_pixel_ptr + cinfo.next_scanline * vinfo.xres * 3;
+        (void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
     }
-
-    free(rgb_row_buffer);
 
     jpeg_finish_compress(&cinfo);
     fclose(outfile);
     jpeg_destroy_compress(&cinfo);
-    munmap(fb_buff, fb_size);
+    free(rgb888_buffer);
+    munmap(fb_data, fb_size);
     close(fbfd);
+
     return 0;
 }
