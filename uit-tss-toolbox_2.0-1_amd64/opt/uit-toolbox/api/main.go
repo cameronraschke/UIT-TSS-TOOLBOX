acbc6fdc3299921a9043787b77abe05b96874ae1
cameronraschke <110272354+cameronraschke@users.noreply.github.com>
2025-09-05 19:25:18 -0500

    Update main.go

--- uit-tss-toolbox_2.0-1_amd64/opt/uit-toolbox/api/main.go
+++ uit-tss-toolbox_2.0-1_amd64/opt/uit-toolbox/api/main.go
@@ -12,6 +12,7 @@ import (
   // "net/http/httputil"
   "time"
   "encoding/json"
+  "encoding/base64"
   // "unicode/utf8"
   "strings"
 	// "crypto/sha256"
@@ -90,6 +91,7 @@ type BearerToken struct {
 type AuthSession struct {
     Basic  BasicToken
     Bearer BearerToken
+    CSRF   string
 }
 
 type returnedJsonToken struct {
@@ -122,6 +124,8 @@ var (
   log = logger.LoggerFactory("console")
 )
 
+
+// Middleware section
 
 func rateLimitMiddleware(next http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
@@ -162,37 +166,40 @@ func rateLimitMiddleware(next http.Handler) http.Handler {
 }
 
 
-func corsMiddleware(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    // Check CORS policy
-    cors := http.NewCrossOriginProtection()
-    cors.AddTrustedOrigin("https://WAN_IP_ADDRESS:1411")
-    if err := cors.Check(req); err != nil {
-      log.Warning("Request to " + req.URL.RequestURI() + " blocked from " + req.RemoteAddr)
+func tlsMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+    if req.TLS == nil || !req.TLS.HandshakeComplete {
+      log.Warning("TLS handshake failed for client " + req.RemoteAddr)
+      http.Error(w, formatHttpError("TLS handshake failed"), http.StatusForbidden)
+      return
+    }
+    next.ServeHTTP(w, req)
+  })
+}
+
+
+func httpMethodMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+    // Get IP address
+    requestIP, _, err := net.SplitHostPort(req.RemoteAddr)
+    if err != nil {
+      log.Warning("Cannot parse IP: " + req.RemoteAddr)
+      http.Error(w, formatHttpError("Bad request"), http.StatusBadRequest)
       return
     }
 
-    w.Header().Set("Access-Control-Allow-Origin", "https://WAN_IP_ADDRESS:1411")
-    w.Header().Set("Access-Control-Allow-Credentials", "true")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie, credentials")
-    w.Header().Set("X-Content-Type-Options", "nosniff")
-    w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-    w.Header().Set("Pragma", "no-cache")
-    w.Header().Set("Expires", "0")
-    w.Header().Set("Host", "WAN_IP_ADDRESS:31411")
-    w.Header().Set("X-Frame-Options", "DENY")
-    w.Header().Set("Content-Security-Policy", "frame-ancestors 'self'")
-    w.Header().Set("Strict-Transport-Security", "max-age=86400; includeSubDomains")
-    w.Header().Set("X-Accel-Buffering", "no")
-
-    // JSON or SSE response
-    parsedURL, _ := url.Parse(req.URL.RequestURI())
-    queries, _ := url.ParseQuery(parsedURL.RawQuery)
-    if queries.Get("sse") == "true" {
-      w.Header().Set("Content-Type", "text/event-stream")
-    } else {
-      w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    // Check method
+    validMethods := map[string]bool{
+      http.MethodOptions: true,
+      http.MethodGet:     true,
+      http.MethodPost:    true,
+      http.MethodPut:     true,
+      http.MethodDelete:  true,
+    }
+    if !validMethods[req.Method] {
+      log.Warning("Invalid request method (" + requestIP + "): " + req.Method)
+      http.Error(w, formatHttpError("Method not allowed"), http.StatusMethodNotAllowed)
+      return
     }
 
     // Handle OPTIONS early
@@ -206,29 +213,51 @@ func corsMiddleware(next http.Handler) http.Handler {
       return
     }
 
-    next.ServeHTTP(w, req)
-  })
-}
-
-
-func tlsMiddleware(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-    if req.TLS == nil || !req.TLS.HandshakeComplete {
-      log.Warning("TLS handshake failed for client " + req.RemoteAddr)
-      http.Error(w, formatHttpError("TLS handshake failed"), http.StatusForbidden)
-      return
+    
+    // Check Content-Type for POST/PUT
+    if req.Method == http.MethodPost || req.Method == http.MethodPut {
+      contentType := req.Header.Get("Content-Type")
+      if contentType != "application/x-www-form-urlencoded" && contentType != "application/json" {
+        log.Warning("Invalid Content-Type header: " + contentType + " (" + requestIP + ": " + req.Method + " " + req.URL.RequestURI() + ")")
+        http.Error(w, formatHttpError("Invalid content type"), http.StatusUnsupportedMediaType)
+        return
+      }
     }
     next.ServeHTTP(w, req)
   })
 }
 
 
-func headersAndMethodMiddleware(next http.Handler) http.Handler {
+func csrfMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+    // Only check for state-changing methods
+    if req.Method == http.MethodPost || req.Method == http.MethodPut || req.Method == http.MethodPatch || req.Method == http.MethodDelete {
+      csrfToken := req.Header.Get("X-CSRF-Token")
+      sessionToken := ""
+      csrfCookie, err := req.Cookie("csrf_token")
+      if err == nil {
+        sessionToken = csrfCookie.Value
+      }
+
+      if csrfToken == "" || sessionToken == "" || csrfToken != sessionToken {
+        http.Error(w, "Invalid or missing CSRF token", http.StatusForbidden)
+        return
+      }
+
+      // Add csrf token to authMap
+      
+    }
+    next.ServeHTTP(w, req)
+  })
+}
+
+
+func checkHeadersMiddleware(next http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
     // Get IP address
-    requestIP, _, err := net.SplitHostPort(requestIP)
+    requestIP, _, err := net.SplitHostPort(req.RemoteAddr)
     if err != nil {
-      log.Warning("Cannot parse IP: " + requestIP)
+      log.Warning("Cannot parse IP: " + req.RemoteAddr)
       http.Error(w, formatHttpError("Bad request"), http.StatusBadRequest)
       return
     }
@@ -302,30 +331,60 @@ func headersAndMethodMiddleware(next http.Handler) http.Handler {
       }
     }
 
-    // Check method
-    validMethods := map[string]bool{
-      http.MethodOptions: true,
-      http.MethodGet:     true,
-      http.MethodPost:    true,
-      http.MethodPut:     true,
-      http.MethodDelete:  true,
-    }
-    if !validMethods[req.Method] {
-      log.Warning("Invalid request method (" + requestIP + "): " + req.Method)
-      http.Error(w, formatHttpError("Method not allowed"), http.StatusMethodNotAllowed)
-      return
-    }
-    // Check Content-Type for POST/PUT
-    if req.Method == http.MethodPost || req.Method == http.MethodPut {
-      contentType := req.Header.Get("Content-Type")
-      if contentType != "application/x-www-form-urlencoded" && contentType != "application/json" {
-        log.Warning("Invalid Content-Type header: " + contentType + " (" + requestIP + ": " + req.Method + " " + req.URL.RequestURI() + ")")
-        http.Error(w, formatHttpError("Invalid content type"), http.StatusUnsupportedMediaType)
-        return
-      }
-    }
     next.ServeHTTP(w, req)
   })
+}
+
+
+func setHeadersMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    // Check CORS policy
+    cors := http.NewCrossOriginProtection()
+    cors.AddTrustedOrigin("https://WAN_IP_ADDRESS:1411")
+    if err := cors.Check(req); err != nil {
+      log.Warning("Request to " + req.URL.RequestURI() + " blocked from " + req.RemoteAddr)
+      return
+    }
+
+    w.Header().Set("Access-Control-Allow-Origin", "https://WAN_IP_ADDRESS:1411")
+    w.Header().Set("Access-Control-Allow-Credentials", "true")
+    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie, credentials")
+    w.Header().Set("X-Content-Type-Options", "nosniff")
+    w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+    w.Header().Set("Pragma", "no-cache")
+    w.Header().Set("Expires", "0")
+    w.Header().Set("Host", "WAN_IP_ADDRESS:31411")
+    w.Header().Set("X-Frame-Options", "DENY")
+    w.Header().Set("Content-Security-Policy", "frame-ancestors 'self'")
+    w.Header().Set("Strict-Transport-Security", "max-age=86400; includeSubDomains")
+    w.Header().Set("X-Accel-Buffering", "no")
+    w.Header().Set("Referrer-Policy", "no-referrer")
+
+    // JSON or SSE response
+    parsedURL, _ := url.Parse(req.URL.RequestURI())
+    queries, _ := url.ParseQuery(parsedURL.RawQuery)
+    if queries.Get("sse") == "true" {
+      w.Header().Set("Content-Type", "text/event-stream")
+    } else {
+      w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    }
+
+    next.ServeHTTP(w, req)
+  })
+}
+
+
+// Helper functions section
+
+func formatHttpError(errorString string) (jsonErrStr string) {
+  jsonStr := httpErrorCodes{Message: errorString}
+  jsonErr, err := json.Marshal(jsonStr)
+  if err != nil {
+    log.Error("Cannot parse JSON: " + err.Error())
+    return
+  }
+  return string(jsonErr)
 }
 
 
@@ -394,14 +453,13 @@ func rateLimitCheck(ipAddrChan <-chan string, bannedChan chan<- bool, rateLimitC
 }
 
 
-func formatHttpError(errorString string) (jsonErrStr string) {
-  jsonStr := httpErrorCodes{Message: errorString}
-  jsonErr, err := json.Marshal(jsonStr)
+func generateCSRFToken() (string, error) {
+  b := make([]byte, 32)
+  _, err := rand.Read(b)
   if err != nil {
-    log.Error("Cannot parse JSON: " + err.Error())
-    return
+    return "", err
   }
-  return string(jsonErr)
+  return base64.StdEncoding.EncodeToString(b), nil
 }
 
 
@@ -697,8 +755,14 @@ func getNewBearerToken(w http.ResponseWriter, req *http.Request) {
 
     basic := BasicToken{Token: basicToken, Expiry: basicExpiry, NotBefore: time.Now(), TTL: basicExpiry.Sub(time.Now()).Seconds(), IP: requestIP, Valid: true}
     bearer := BearerToken{Token: bearerToken, Expiry: bearerExpiry, NotBefore: time.Now(), TTL: bearerExpiry.Sub(time.Now()).Seconds(), IP: requestIP, Valid: true}
+    csrfToken, err := generateCSRFToken()
+    if err != nil {
+      log.Error("Cannot generate CSRF token: " + err.Error())
+      http.Error(w, formatHttpError("Internal server error"), http.StatusInternalServerError)
+      return
+    }
     _, exists := authMap.Load(sessionID)
-    authMap.Store(sessionID, AuthSession{Basic: basic, Bearer: bearer})
+    authMap.Store(sessionID, AuthSession{Basic: basic, Bearer: bearer, CSRF: csrfToken})
 
     value, ok := authMap.Load(sessionID)
     if !ok {
@@ -706,6 +770,16 @@ func getNewBearerToken(w http.ResponseWriter, req *http.Request) {
       http.Error(w, formatHttpError("Internal server error"), http.StatusInternalServerError)
       return
     }
+
+    http.SetCookie(w, &http.Cookie{
+      Name:     "csrf_token",
+      Value:    csrfToken,
+      Path:     "/",
+      Secure:   true,
+      HttpOnly: false,
+      SameSite: http.SameSiteStrictMode,
+    })
+
     authSession := value.(AuthSession)
 
     if authSession.Bearer.Token != bearerToken || authSession.Bearer.TTL <= 0 || 
@@ -750,182 +824,6 @@ func getNewBearerToken(w http.ResponseWriter, req *http.Request) {
     io.WriteString(w, string(jsonData))
     return
   }
-}
-
-
-func apiMiddleWare(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-    var parsedURL *url.URL
-    var queries url.Values
-    var bearerToken string
-    var basicToken string
-    var token string
-    var err error
-
-    // Check CORS policy
-    cors := http.NewCrossOriginProtection()
-    cors.AddTrustedOrigin("https://WAN_IP_ADDRESS:1411")
-    if err = cors.Check(req); err != nil {
-      log.Warning("Request to " + req.URL.RequestURI() + " blocked from " + req.RemoteAddr)
-      return
-    }
-
-    // Check rate limiting map
-    ipAddrChan := make(chan string)
-    bannedChan := make(chan bool)
-    rateLimitChan := make(chan int)
-
-    ip, _, err := net.SplitHostPort(req.RemoteAddr)
-    _, _ = ipMap.LoadOrStore(ip, RateLimiter{Requests: 1, LastSeen: time.Now(), MapLastUpdated: time.Now(), Banned: false})
-
-    //log.Info("Received request (" + req.RemoteAddr + "): " + req.Method + " " + req.URL.RequestURI())
-
-    go func() {
-      rateLimitCheck(ipAddrChan, bannedChan, rateLimitChan)
-    }()
-
-    ipAddrChan <- ip
-    close(ipAddrChan)
-
-    select {
-    case bannedBool := <-bannedChan:
-      if bannedBool == true {
-        reqsPerSec := <-rateLimitChan
-        log.Warning("Banned (" + ip + ")" + ", too many requests (" + fmt.Sprintf("%d", reqsPerSec) + "/s)")
-        // http.Error(w, formatHttpError("Too many requests"), http.StatusTooManyRequests)
-        return
-      }
-    case <-time.After(5 * time.Second):
-      log.Error("Ban check timed out")
-    }
-
-    // Check if TLS connection is valid
-    if req.TLS.HandshakeComplete == false && req.TLS.DidResume == false {
-      log.Warning("TLS handshake failed for client " + req.RemoteAddr)
-      return
-    }
-
-    // Check if request content length exceeds 64 MB
-    if req.ContentLength > 64 << 20 {
-      log.Warning("Request content length exceeds limit: " + fmt.Sprintf("%.2f%s", float64(float64(req.ContentLength) / 1000000), "MB"))
-      return
-    }
-
-
-    // Check if request method is valid
-    if req.Method != http.MethodOptions && req.Method != http.MethodGet && req.Method != http.MethodPost && req.Method != http.MethodPut && req.Method != http.MethodDelete {
-      log.Warning("Invalid request method (" + req.RemoteAddr + "): " + req.Method)
-      return
-    }
-
-
-    // Check if MIME type and Content-Type is valid
-    // mimeType := http.DetectContentType(req.Body)
-    // if mimeType == "application/octet-stream" || (mimeType != "application/x-www-form-urlencoded" && mimeType != "application/json") {
-    //  log.Sprintf("%s %s\n", "Invalid Mime Type: ", mimeType)
-    //  http.Error(w, formatHttpError("Invalid MIME type"), http.StatusUnsupportedMediaType)
-    //  return
-    // }
-    if req.Method != http.MethodOptions && req.Method != http.MethodPost && req.Method != http.MethodPut {
-      if req.Header.Get("Content-Type") != "application/x-www-form-urlencoded" && req.Header.Get("Content-Type") != "application/json" {
-        log.Warning("Invalid Content-Type header: " + req.Header.Get("Content-Type") + " (" + req.RemoteAddr + ": " + req.Method + " " + req.URL.RequestURI() + ")")
-        http.Error(w, formatHttpError("Invalid content type"), http.StatusUnsupportedMediaType)
-        return
-      }
-    }
-
-
-    // Parse URL to get path and queries
-    parsedURL, err = url.Parse(req.URL.RequestURI())
-    if err != nil {
-      log.Warning("Cannot parse URL ( " + req.RemoteAddr + "): " + " " + err.Error() + " (" + req.URL.RequestURI() + ")")
-      return
-    }
-    RawQuery := parsedURL.RawQuery
-    queries, _ = url.ParseQuery(RawQuery)
-
-
-    // Check if headers exist
-    headerCount := 0
-    for key, _ := range req.Header {
-      if len(strings.TrimSpace(key)) > 0 {
-        headerCount++
-      }
-    }
-    if headerCount == 0 {
-      log.Warning("Empty header request from: " + req.RemoteAddr)
-    }
-
-
-    // Headers for OPTIONS request
-    if req.Method == http.MethodOptions {
-      w.Header().Set("Access-Control-Allow-Origin", "https://WAN_IP_ADDRESS:1411")
-      w.Header().Set("Access-Control-Allow-Credentials", "true")
-      w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-      w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie, credentials")
-      w.WriteHeader(http.StatusNoContent)
-      return
-    }
-    
-    // Headers for all other requests
-    if queries.Get("sse") == "true" {
-      w.Header().Set("Content-Type", "text/event-stream")
-    } else {
-      w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    }
-
-    w.Header().Set("Access-Control-Allow-Origin", "https://WAN_IP_ADDRESS:1411")
-    w.Header().Set("Access-Control-Allow-Credentials", "true")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie, credentials")
-    w.Header().Set("Content-Security-Policy", "img-src 'none'")
-    w.Header().Set("Content-Security-Policy", "img-src https://WAN_IP_ADDRESS:31411/api/remote/")
-    w.Header().Set("X-Content-Type-Options", "nosniff")
-    w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-    w.Header().Set("Pragma", "no-cache")
-    w.Header().Set("Expires", "0")
-    w.Header().Set("Host", "WAN_IP_ADDRESS:31411")
-    w.Header().Set("X-Frame-Options", "DENY")
-    w.Header().Set("Content-Security-Policy", "frame-ancestors 'self'")
-    w.Header().Set("Strict-Transport-Security", "max-age=86400; includeSubDomains")
-    w.Header().Set("X-Accel-Buffering", "no")
-    w.WriteHeader(http.StatusOK)
-
-
-    // Check if Authorization header exists
-    authHeaderMap := req.Header.Values("authorization")
-    if len(authHeaderMap) >= 1 {
-      for _, value := range authHeaderMap {
-        if strings.HasPrefix(value, "Bearer ") {
-          bearerToken = strings.TrimPrefix(value, "Bearer ")
-        } else if strings.HasPrefix(value, "Basic ") {
-          basicToken = strings.TrimPrefix(value, "Basic ")
-        } else {
-          log.Info("Missing/Malformed Authorization header")
-          // http.Error(w, formatHttpError("Bad request"), http.StatusBadRequest)
-          // return
-        }
-
-        if bearerToken != "" {
-          token = bearerToken
-        } else if basicToken != "" {
-          token = basicToken
-        }
-
-        // Check if the token is empty
-        if len(strings.TrimSpace(token)) == 0 {
-          log.Warning("Empty value for Authorization header")
-          http.Error(w, formatHttpError("Empty Authorization header"), http.StatusUnauthorized)
-          return
-        }
-      }
-    } else {
-      log.Info("Authorization header missing: " + req.URL.RequestURI())
-    }
-
-    // Don't call next.ServeHTTP(w, req) because this is first function in muxChain
-    next.ServeHTTP(w, req)
-  })
 }
 
 
@@ -1186,9 +1084,11 @@ func main() {
   // Route to correct function
   baseMuxChain := muxChain{
     rateLimitMiddleware,
-    corsMiddleware,
     tlsMiddleware,
-    headersAndMethodMiddleware,
+    httpMethodMiddleware,
+    csrfMiddleware,
+    checkHeadersMiddleware,
+    setHeadersMiddleware,
     apiAuth,
   }
   // refreshTokenMuxChain := muxChain{apiMiddleWare}
