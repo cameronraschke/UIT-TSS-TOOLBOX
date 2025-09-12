f45fe5912154e31ff6f97a3bc6eee564f28d258f
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2025-09-12 10:40:10 -0500

    Update footer.js

--- uit-tss-toolbox_2.0-1_amd64/opt/uit-toolbox/api/static/js/footer.js
+++ uit-tss-toolbox_2.0-1_amd64/opt/uit-toolbox/api/static/js/footer.js
@@ -1,5 +1,9 @@
 
-if (document.getElementById("uit-footer")) {
-    const footer = await fetchData("/api/static/html/footer.html");
-    if (footer) document.getElementById("uit-footer").innerHTML = footer;
+async function drawFooter() {
+    try {
+        const footer = await fetchData("/api/static/html/footer.html");
+        if (footer) document.getElementById("uit-footer").innerHTML = footer;
+    } catch (error) {
+        console.error("Error fetching footer:", error);
+    }
 }
